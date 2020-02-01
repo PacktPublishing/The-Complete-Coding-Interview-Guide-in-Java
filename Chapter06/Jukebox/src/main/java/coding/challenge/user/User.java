b59d0219635178516a428eadafa92b91eac02296
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-01 10:46:34 +0200

    Jukebox

--- Chapter06/Jukebox/src/main/java/coding/challenge/user/User.java
+++ Chapter06/Jukebox/src/main/java/coding/challenge/user/User.java
@@ -3,7 +3,7 @@ package coding.challenge.user;
 import coding.challenge.jukebox.Jukebox;
 
 public class User {
-    private Jukebox jukebox;
+    private final Jukebox jukebox;
 
     public User(Jukebox jukebox) {
         this.jukebox = jukebox;
