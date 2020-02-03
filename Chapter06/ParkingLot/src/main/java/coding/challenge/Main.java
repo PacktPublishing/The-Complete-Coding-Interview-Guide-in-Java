322576e8cecb2bad2b240598a2158d78aeabf24c
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-03 14:00:05 +0200

    Parking lot

--- Chapter06/ParkingLot/src/main/java/coding/challenge/Main.java
+++ Chapter06/ParkingLot/src/main/java/coding/challenge/Main.java
@@ -1,6 +1,5 @@
 package coding.challenge;
 
-import coding.challenge.actor.Driver;
 import coding.challenge.parking.ParkingFloor;
 import coding.challenge.parking.ParkingLot;
 import coding.challenge.parking.ParkingTicket;
@@ -14,15 +13,14 @@ public class Main {
     public static void main(String[] args) {
 
         Vehicle van = new Van("4592-EE");
-        Driver driverVan = new Driver("Mark Jabel", van);
-
+        
         ParkingFloor aqua = new ParkingFloor("Aqua", 25);
         ParkingFloor magenta = new ParkingFloor("Magenta", 35);
         ParkingLot parkingLot = new ParkingLot("SkyParking", List.of(aqua, magenta));
         
         ParkingSystem parkingSystem = new ParkingSystem("1", parkingLot);
         
-        // driverVan parks a car via ParkingSystem
+        // van's driver parks a car via ParkingSystem
         ParkingTicket parkingTicket = parkingSystem.parkVehicle(van);
         
         System.out.println("Parking ticket: " + parkingTicket);
