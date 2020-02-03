b670f536b209018f62237bf3a47b61bd863e4b3b
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-03 11:25:53 +0200

    Tuple

---
+++ Chapter06/Tuple/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,13 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Pair pair = Pair.of("left", "right");
+       
+       System.out.println(pair.left);
+       System.out.println(pair.right);
+    }
+
+}
