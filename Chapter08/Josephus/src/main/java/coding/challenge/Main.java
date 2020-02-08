8a77ca2eeb7228b6e142285233c27dafe7bb8732
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-08 08:53:57 +0200

    Josephus

--- Chapter08/Josephus/src/main/java/coding/challenge/Main.java
+++ Chapter08/Josephus/src/main/java/coding/challenge/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // 41 people, eliminating every 3th person until only one person is left
+        // the survivor should be number 31
+        
         Josephus josephus = new Josephus();
         System.out.println("Using recursion! Survivor: " + josephus.josephus(41, 3) + "\n");
        
