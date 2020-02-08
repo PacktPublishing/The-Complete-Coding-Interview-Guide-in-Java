b872c7cf9d074e89fa18810ce63d334477666c93
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-08 11:46:03 +0200

    ColorSpots

---
+++ Chapter08/ColorSpots/src/main/java/coding/challenge/BiggestColorSpot.java
@@ -0,0 +1,46 @@
+package coding.challenge;
+
+public class BiggestColorSpot {
+
+    private int currentColorSpot;
+
+    void printBiggestColorSpot(int cols, int rows, int a[][]) {
+
+        int biggestColorSpot = 0;
+        int color = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (a[i][j] > 0) {
+                    currentColorSpot = 0;
+                    computeBiggestColorSpot(i, j, cols, rows, a, a[i][j]);
+                }
+                if (currentColorSpot > biggestColorSpot) {
+                    biggestColorSpot = currentColorSpot;
+                    color = a[i][j] * (-1);
+                }
+            }
+        }
+
+        System.out.println("\nColor: " + color + " Bigest spot: " + biggestColorSpot);
+    }
+
+    private void computeBiggestColorSpot(int i, int j, int cols, int rows, int a[][], int color) {
+
+        a[i][j] = -a[i][j];
+        currentColorSpot++;
+
+        if (i > 1 && a[i - 1][j] == color) {
+            computeBiggestColorSpot(i - 1, j, cols, rows, a, color);
+        }
+        if ((i + 1) < rows && a[i + 1][j] == color) {
+            computeBiggestColorSpot(i + 1, j, cols, rows, a, color);
+        }
+        if (j > 1 && a[i][j - 1] == color) {
+            computeBiggestColorSpot(i, j - 1, cols, rows, a, color);
+        }
+        if ((j + 1) < cols && a[i][j + 1] == color) {
+            computeBiggestColorSpot(i, j + 1, cols, rows, a, color);
+        }
+    }
+}
---
+++ Chapter08/ColorSpots/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,36 @@
+package coding.challenge;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int cols = 5;
+        int rows = 5;
+        int colors = 3; // a spot can have the colors 1, 2 or 3
+        
+        Random rnd = new Random();
+
+        int[][] a = new int[rows][cols];
+
+        // add random colors
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = 1 + rnd.nextInt(colors);
+            }
+        }
+
+        // show the surface on screen
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(a[i][j] + "  ");
+            }
+            System.out.println();
+        }
+
+        BiggestColorSpot spots = new BiggestColorSpot();
+        spots.printBiggestColorSpot(cols, rows, a);                
+    }
+
+}
