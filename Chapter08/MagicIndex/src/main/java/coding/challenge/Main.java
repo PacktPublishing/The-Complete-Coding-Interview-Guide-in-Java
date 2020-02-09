7421debb3249509f90b64c0afbcd5de8eaa08244
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-09 17:25:12 +0200

    Magic index

--- Chapter08/MagicIndex/src/main/java/coding/challenge/Main.java
+++ Chapter08/MagicIndex/src/main/java/coding/challenge/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         MagicIndex mi = new MagicIndex();
-        int[] arr = new int[]{1, 4, 4, 4, 6, 7, 7, 9, 10, 11, 12, 12, 12, 15, 17, 20, 21, 21};
+        int[] arr = new int[]{1, 4, 4, 4, 5, 5, 6, 6, 6, 11, 12, 12, 12, 15, 17, 20, 21, 21};
         int resultIndex = mi.findMagicIndex(arr);
         
         System.out.println("Index " + resultIndex + " has value " + arr[resultIndex]);        
