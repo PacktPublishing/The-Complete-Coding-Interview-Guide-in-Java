317fe33beb84e22d196e277c6d2f0e8e58e05b3a
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-11 09:53:13 +0200

    Knight tour

---
+++ Chapter08/KnightTour/src/main/java/coding/challenge/KnightTour.java
@@ -0,0 +1,60 @@
+package coding.challenge;
+
+public class KnightTour {
+
+    private final int n;
+
+    public KnightTour(int n) {
+        this.n = n;
+    }        
+    
+    // all 8 possible movements for a knight
+    public static final int COL[] = {1, 2, 2, 1, -1, -2, -2, -1, 1};
+    public static final int ROW[] = {2, 1, -1, -2, -2, -1, 1, 2, 2};
+  
+    public void knightTour(int r, int c, int cell, int visited[][]) {
+        
+        // mark current square as visited
+        visited[r][c] = cell;
+
+        // we have a solution
+        if (cell >= n * n) {
+            print(visited);
+            // backtrack before returning
+            visited[r][c] = 0;
+            return;
+        }
+
+        // check for all possible movements (8) and recur for each valid movement
+        for (int i = 0; i < (ROW.length - 1); i++) {
+                        
+            int newR = r + ROW[i];
+            int newC = c + COL[i];
+
+            // check if the new position is valid un-visited
+            if (isValid(newR, newC) && visited[newR][newC] == 0) {
+                knightTour(newR, newC, cell + 1, visited);
+            }
+        }
+
+        // backtrack from current square and remove it from current path
+        visited[r][c] = 0;
+    }
+
+    // check if (r, c) is valid chess board coordinates    
+    private boolean isValid(int r, int c) {        
+        return !(r < 0 || c < 0 || r >= n || c >= n);
+    }
+
+    // print the solution as a board
+    private void print(int[][] visited) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.format("%3s", visited[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+}
---
+++ Chapter08/KnightTour/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,21 @@
+package coding.challenge;
+
+public class Main {
+    
+    public static final int GRID_SIZE = 5; // minim 5
+    
+    public static void main(String[] args) {
+
+        KnightTour knightTour = new KnightTour(GRID_SIZE);
+        
+        // visited[][] serves two purpose -
+        // 1. It keep track of squares involved the Knight's tour.
+        // 2. It stores the order in which the squares are visited
+        int visited[][] = new int[GRID_SIZE][GRID_SIZE];
+        int cell = 1;
+
+        // start knight tour from corner square (0, 0)
+        knightTour.knightTour(0, 0, cell, visited);       
+    }
+
+}
