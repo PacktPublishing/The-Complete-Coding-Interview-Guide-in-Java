1a07e97fe613a40a0c462538d98575d412147c01
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-13 16:47:28 +0200

    Insert P bits in Q bits

---
+++ Chapter09/InsertPInQ/src/main/java/coding/challenge/Insertion.java
@@ -0,0 +1,18 @@
+package coding.challenge;
+
+public class Insertion {
+
+    int updateBits(int q, int p, int i, int j) {
+        int ones = ~0; // 11111111 11111111 11111111 11111111          
+        
+        int leftShiftJ = ones << (j + 1); 
+        int leftShiftI = ((1 << i) - 1); 
+        
+        int mask = leftShiftJ | leftShiftI; 
+        
+        int applyMaskToQ = q & mask;                        
+        int bringQInPlace = p << i;        
+
+        return applyMaskToQ | bringQInPlace; 
+    }
+}
