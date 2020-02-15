0d3da28ce8a4f82b7407eebcedde721ae04639f1
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-15 11:25:17 +0200

    Get bit value

--- Chapter09/GetBitValue/src/main/java/coding/challenge/Bits.java
+++ Chapter09/GetBitValue/src/main/java/coding/challenge/Bits.java
@@ -6,14 +6,14 @@ public final class Bits {
         throw new AssertionError("Cannot be instantiated");
     }
 
-    public static char getBitValue(int n, int position) {
+    public static char getBitValue(int n, int k) {
 
-        if (position < 0 || position > 32) {
+        if (k < 0 || k > 32) {
             throw new IllegalArgumentException("The position must be between 0 and 32");
         }
 
-        int result = n & (1 << position);
-        // or, int result = 1 & (n >> position);
+        int result = n & (1 << k);
+        // or, int result = 1 & (n >> k);
 
         if (result == 0) {
             return '0';
