3c6e9f06f159d35610a32ed96b143413679eb71c
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-16 08:33:07 +0200

    Set bit value

--- Chapter09/SetBitValue/src/main/java/coding/challenge/Bits.java
+++ Chapter09/SetBitValue/src/main/java/coding/challenge/Bits.java
@@ -6,7 +6,7 @@ public final class Bits {
         throw new AssertionError("Cannot be instantiated");
     }
 
-    public static int setBitValueTo1(int n, int k) {
+    public static int setValueTo1(int n, int k) {
 
         if (k < 0 || k > 32) {
             throw new IllegalArgumentException("The position must be between 0 and 32");
@@ -15,7 +15,7 @@ public final class Bits {
         return n | (1 << k);
     }
     
-    public static int setBitValueTo0(int n, int k) {
+    public static int setValueTo0(int n, int k) {
 
         if (k < 0 || k > 32) {
             throw new IllegalArgumentException("The position must be between 0 and 32");
--- Chapter09/SetBitValue/src/main/java/coding/challenge/Main.java
+++ Chapter09/SetBitValue/src/main/java/coding/challenge/Main.java
@@ -7,13 +7,13 @@ public class Main {
         int number = 423;
         int position = 7;
 
-        int result0 = Bits.setBitValueTo0(number, position);
+        int result0 = Bits.setValueTo0(number, position);
 
         System.out.println("\nSetting bit on position " + position + " to 0:");
         System.out.println("Number: " + number + "(" + Integer.toBinaryString(number) + ")");
         System.out.println("Result: " + result0 + "(" + Integer.toBinaryString(result0) + ")");
         
-        int result1 = Bits.setBitValueTo1(result0, position);
+        int result1 = Bits.setValueTo1(result0, position);
 
         System.out.println("\nSetting bit  on position " + position + " to 1:");
         System.out.println("Number: " + result0 + "(" + Integer.toBinaryString(result0) + ")");
