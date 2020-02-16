97637fb48450fb57c768a485c04f0b68bb6bac9e
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-16 10:37:22 +0200

    Summing binaries

--- Chapter09/SummingBinaries/src/main/java/coding/challenge/Bits.java
+++ Chapter09/SummingBinaries/src/main/java/coding/challenge/Bits.java
@@ -6,19 +6,22 @@ public final class Bits {
         throw new AssertionError("Cannot be instantiated");
     }
 
+    // using the equation, p + q = 2 * (p & q) + (p ^ q)
     public static int sum(int q, int p) {
 
         int xor;
         int and;
         int t;
-
+        
         and = q & p;
         xor = q ^ p;
 
+        // force and to return 0
         while (and != 0) {
 
-            and = and << 1;
+            and = and << 1; // this is multiplication by 2
 
+            // prepare the next step of recursion
             t = xor ^ and;
             and = and & xor;
             xor = t;
