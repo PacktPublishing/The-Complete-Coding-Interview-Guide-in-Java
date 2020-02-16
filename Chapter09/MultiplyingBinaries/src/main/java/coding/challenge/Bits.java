01646f070e563fb98e3add4f32dfd97c58c6c157
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-16 13:12:41 +0200

    Multiplying binaries

---
+++ Chapter09/MultiplyingBinaries/src/main/java/coding/challenge/Bits.java
@@ -0,0 +1,26 @@
+package coding.challenge;
+
+public final class Bits {
+    
+    private Bits() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int multiply(int q, int p) {
+
+        int result = 0;
+        
+        while (p != 0) {
+
+            // we compute the value of q only when the LSB of p is 1            
+            if ((p & 1) != 0) {
+                result = result + q;                
+            }
+
+            q = q << 1;  // q is left shifted with one position
+            p = p >>> 1; // p is logical right shifted with one position
+        }
+
+        return result;
+    }
+}
--- Chapter09/MultiplyingBinaries/src/main/java/coding/challenge/Main.java
+++ Chapter09/MultiplyingBinaries/src/main/java/coding/challenge/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
     public static void main(String[] args) {   
         
-        Multiply multiply = new Multiply();
-        
-        int result = multiply.multiplyBits(23, 45);
-        
-        System.out.println("Result: " + result + "(" + Integer.toString(result, 2) + ")");
+        int q = 124;
+        int p = 29;
+
+        int result = Bits.multiply(q, p);
+
+        System.out.println(q + "(" + Integer.toBinaryString(q) + ")" + " * "
+                + p + "(" + Integer.toBinaryString(p) + ")" + " = " + result
+                + "(" + Integer.toBinaryString(result) + ")");
     }
 
 }
