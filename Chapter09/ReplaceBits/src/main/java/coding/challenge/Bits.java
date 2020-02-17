de5c9a8da49cf008bf9ccea1dfb01ab6ea69d389
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-17 14:52:18 +0200

    Replace bits

--- Chapter09/ReplaceBits/src/main/java/coding/challenge/Bits.java
+++ Chapter09/ReplaceBits/src/main/java/coding/challenge/Bits.java
@@ -1,22 +1,27 @@
 package coding.challenge;
 
-public final class Bits {   
-    
+public final class Bits {
+
     private Bits() {
         throw new AssertionError("Cannot be instantiated");
     }
-    
-    public static int replace(int q, int p, int i, int j) {
-        int ones = ~0; // 11111111 11111111 11111111 11111111          
-        
-        int leftShiftJ = ones << (j + 1); 
-        int leftShiftI = ((1 << i) - 1); 
-        
-        int mask = leftShiftJ | leftShiftI; 
-        
-        int applyMaskToQ = q & mask;                        
-        int bringPInPlace = p << i;        
 
-        return applyMaskToQ | bringPInPlace; 
+    public static int replace(int q, int p, int i, int j) {
+
+        if (i < 0 || j < 0 || i > 31 || j > 31 || i >= j) {
+            throw new IllegalArgumentException("The i and j values don't follow the problem instructions");
+        }
+
+        int ones = ~0; // 11111111 11111111 11111111 11111111          
+
+        int leftShiftJ = ones << (j + 1);
+        int leftShiftI = ((1 << i) - 1);
+
+        int mask = leftShiftJ | leftShiftI;
+
+        int applyMaskToQ = q & mask;
+        int bringPInPlace = p << i;
+
+        return applyMaskToQ | bringPInPlace;
     }
 }
