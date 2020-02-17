a3b040c1d825ec76f25c96139d137a71f1c07152
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-17 11:39:51 +0200

    Dividing binaries

--- Chapter09/DividingBinaries/src/main/java/coding/challenge/Bits.java
+++ Chapter09/DividingBinaries/src/main/java/coding/challenge/Bits.java
@@ -3,12 +3,51 @@ package coding.challenge;
 public final class Bits {
 
     private static final int MAX_BIT = 31;
-    
+
     private Bits() {
         throw new AssertionError("Cannot be instantiated");
     }
 
-    public static long divide(long q, long p) {
+    public static long divideWithRemainder(long q, long p) {
+
+        if (p == 0) {
+            throw new ArithmeticException("Division by 0");
+        }
+
+        // obtain the sign of the division
+        long sign = ((q < 0) ^ (p < 0)) ? -1 : 1;
+
+        // ensure that q and p are positive
+        q = Math.abs(q);
+        p = Math.abs(p);
+
+        long t = 1;
+        long quotient = 0;
+
+        while (p <= q) {
+            p = p << 1;
+            t = t << 1;
+        }
+
+        while (t > 1) {
+            p = p >> 1;
+            t = t >> 1;
+            if (q >= p) {
+                q -= p;
+                quotient = quotient | t;
+            }
+        }
+
+        System.out.println("Remainder is: " + q);
+
+        return sign * quotient;
+    }
+
+    public static long divideWithoutRemainder(long q, long p) {
+
+        if (p == 0) {
+            throw new ArithmeticException("Division by 0");
+        }
 
         // obtain the sign of the division
         long sign = ((q < 0) ^ (p < 0)) ? -1 : 1;
@@ -18,16 +57,15 @@ public final class Bits {
         p = Math.abs(p);
 
         long t = 0;
-        long quotient = 0;        
+        long quotient = 0;
 
-        // 
         for (int i = MAX_BIT; i >= 0; --i) {
-            
-            long halfdown = t + (p << i);              
-            
+
+            long halfdown = t + (p << i);
+
             if (halfdown <= q) {
-                
-                t = t +  p << i;
+
+                t = t + p << i;
                 quotient = quotient | 1L << i;
             }
         }
--- Chapter09/DividingBinaries/src/main/java/coding/challenge/Main.java
+++ Chapter09/DividingBinaries/src/main/java/coding/challenge/Main.java
@@ -4,16 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int q = 11;
-        int p = 2;
+        int q = 23;
+        int p = 7;
 
-        long result = Bits.divide(q, p);
-
+        long result1 = Bits.divideWithRemainder(q, p);        
         System.out.println(q + "(" + Long.toBinaryString(q) + ")" + " / "
-                + p + "(" + Long.toBinaryString(p) + ")" + " = " + result
-                + "(" + Long.toBinaryString(result) + ")");
-
-        System.out.println();
+                + p + "(" + Long.toBinaryString(p) + ")" + " = " + result1
+                + "(" + Long.toBinaryString(result1) + ")");
+        
+        System.out.println("-------------------------------------------------");
+        
+        long result2 = Bits.divideWithoutRemainder(q, p);
+        System.out.println(q + "(" + Long.toBinaryString(q) + ")" + " / "
+                + p + "(" + Long.toBinaryString(p) + ")" + " = " + result2
+                + "(" + Long.toBinaryString(result2) + ")");        
     }
 
 }
