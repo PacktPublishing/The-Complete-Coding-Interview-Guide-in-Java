5c4ecbc01a348009af132864c82de6a974d873c0
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-19 18:18:53 +0200

    Robot grid maze

--- Chapter08/RobotGridMaze/src/main/java/coding/challenge/RobotGrid.java
+++ Chapter08/RobotGridMaze/src/main/java/coding/challenge/RobotGrid.java
@@ -13,8 +13,8 @@ public final class RobotGrid {
     // we go recursively from [m, n] (top-left corner) and try to reach [0, 0] (bottom-right corner)
     public static boolean computePath(int m, int n, boolean[][] maze, Set<Point> path) {
 
-        if (path == null) {
-            throw new IllegalArgumentException("Path cannot be null");
+        if (path == null || maze == null) {
+            throw new IllegalArgumentException("Path and maze cannot be null");
         }
 
         // we fell off the grid so we return
@@ -44,8 +44,8 @@ public final class RobotGrid {
     // we go recursively from [m, n](top-left corner) and try to reach [0, 0] (bottom-right corner)
     public static boolean computePath(int m, int n, boolean[][] maze, Set<Point> path, Set<Point> visitFailed) {
 
-        if (path == null) {
-            throw new IllegalArgumentException("Path cannot be null");
+        if (path == null || maze ==null || visitFailed == null) {
+            throw new IllegalArgumentException("Path, maze and visitFailed cannot be null");
         }
         
         // we fell off the grid so we return
