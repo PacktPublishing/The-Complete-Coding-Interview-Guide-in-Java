83758b5cc112f7989cbc0e0a01d9441d1d4830ec
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-19 17:13:16 +0200

    Count bits to flip in q in order to convert q to p

--- Chapter09/Conversion/src/main/java/coding/challenge/Bits.java
+++ Chapter09/Conversion/src/main/java/coding/challenge/Bits.java
@@ -8,6 +8,10 @@ public final class Bits {
 
     public static int count(int q, int p) {
 
+        if (q < 0 || p < 0) {
+            throw new IllegalArgumentException("The q and p numbers must be positive");
+        }
+
         int count = 0;
 
         int xor = q ^ p; // each 1 represents a bit that is different between q and p
