41b38c73564a9a79a81720f941d83b196cfe8fca
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-19 08:55:41 +0200

    Swap odd and even bits

---
+++ Chapter09/SwapOddEven/src/main/java/coding/challenge/Bits.java
@@ -0,0 +1,16 @@
+package coding.challenge;
+
+public final class Bits {
+
+    private Bits() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int swap(int n) {
+
+        int moveToEvenPositions = (n & 0b10101010101010101010101010101010) >>> 1;
+        int moveToOddPositions = (n & 0b1010101010101010101010101010101) << 1;
+
+        return moveToEvenPositions | moveToOddPositions;
+    }
+}
---
+++ Chapter09/SwapOddEven/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,15 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 663;        
+
+        int result = Bits.swap(number);
+        
+        System.out.println("Initial:\t\t" + Integer.toBinaryString(number));
+        System.out.println("Swapped:\t\t" + Integer.toBinaryString(result));        
+    }
+
+}
