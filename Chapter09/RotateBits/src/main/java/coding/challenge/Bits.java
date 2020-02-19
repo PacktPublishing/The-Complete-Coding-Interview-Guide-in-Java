65179db3ff2b99e7126204868de357782b5d9863
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-19 10:32:50 +0200

    Rotate bits

---
+++ Chapter09/RotateBits/src/main/java/coding/challenge/Bits.java
@@ -0,0 +1,26 @@
+package coding.challenge;
+
+public final class Bits {
+
+    private static final int MAX_INT_BITS = 32;
+
+    private Bits() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int leftRotate(int n, int bits) {
+
+        int fallBits = n << bits;
+        int fallBitsShiftToRight = n >> (MAX_INT_BITS - bits);
+
+        return fallBits | fallBitsShiftToRight;
+    }
+
+    public static int rightRotate(int n, int bits) {
+
+        int fallBits = n >> bits;
+        int fallBitsShiftToLeft = n << (MAX_INT_BITS - bits);
+
+        return fallBits | fallBitsShiftToLeft;
+    }
+}
---
+++ Chapter09/RotateBits/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,22 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 423099897;
+        int bits = 10;
+
+        int resultLeft = Bits.leftRotate(number, bits);
+        int resultRight = Bits.rightRotate(number, bits);
+
+        System.out.println("Initial:\t" + Integer.toBinaryString(number));
+        System.out.println("Result-left:\t" + Integer.toBinaryString(resultLeft));
+        
+        System.out.println();
+
+        System.out.println("Initial:\t" + Integer.toBinaryString(number));
+        System.out.println("Result-right:\t" + Integer.toBinaryString(resultRight));
+    }
+
+}
