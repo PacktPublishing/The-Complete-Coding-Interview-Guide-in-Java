8475882a329f24430514be410a826f2e84479359
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-19 16:48:38 +0200

    Clear bits

--- Chapter09/ClearBits/src/main/java/coding/challenge/Bits.java
+++ Chapter09/ClearBits/src/main/java/coding/challenge/Bits.java
@@ -9,7 +9,7 @@ public final class Bits {
     public static int clearFromMsb(int n, int k) {
 
         if (k < 0 || k > 31) {
-            throw new IllegalArgumentException("The position must be between 0 and 32");
+            throw new IllegalArgumentException("The position must be between 0 and 31");
         }
 
         return n & ((1 << k) - 1);
@@ -18,7 +18,7 @@ public final class Bits {
     public static int clearFromPosition(int n, int k) {
 
         if (k < 0 || k > 31) {
-            throw new IllegalArgumentException("The position must be between 0 and 32");
+            throw new IllegalArgumentException("The position must be between 0 and 31");
         }
 
         return n & ~((1 << k) - 1);
