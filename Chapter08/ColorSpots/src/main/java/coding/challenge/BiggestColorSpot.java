4710558582ce03e2d4e6e0818128825f2c65d08a
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-19 18:22:13 +0200

    Biggest color spot

--- Chapter08/ColorSpots/src/main/java/coding/challenge/BiggestColorSpot.java
+++ Chapter08/ColorSpots/src/main/java/coding/challenge/BiggestColorSpot.java
@@ -6,17 +6,25 @@ public class BiggestColorSpot {
 
     void determineBiggestColorSpot(int cols, int rows, int a[][]) {
 
+        if (a == null) {
+            throw new IllegalArgumentException("The matrix a cannot be null");
+        }
+
+        if (cols <= 0 || rows <= 0) {
+            throw new IllegalArgumentException("Grid cannot have 0 rows and/or cols!");
+        }
+
         int biggestColorSpot = 0;
         int color = 0;
- 
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                
+
                 if (a[i][j] > 0) {
                     currentColorSpot = 0;
-                    
+
                     computeColorSpot(i, j, cols, rows, a, a[i][j]);
-                    
+
                     if (currentColorSpot > biggestColorSpot) {
                         biggestColorSpot = currentColorSpot;
                         color = a[i][j] * (-1);
@@ -36,15 +44,15 @@ public class BiggestColorSpot {
         if (i > 1 && a[i - 1][j] == color) {
             computeColorSpot(i - 1, j, cols, rows, a, color);
         }
-        
+
         if ((i + 1) < rows && a[i + 1][j] == color) {
             computeColorSpot(i + 1, j, cols, rows, a, color);
         }
-        
+
         if (j > 1 && a[i][j - 1] == color) {
             computeColorSpot(i, j - 1, cols, rows, a, color);
         }
-        
+
         if ((j + 1) < cols && a[i][j + 1] == color) {
             computeColorSpot(i, j + 1, cols, rows, a, color);
         }
