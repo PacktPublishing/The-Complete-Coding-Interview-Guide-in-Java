e86c6882342777be82b7ed709664166fc83773e8
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-19 13:18:21 +0200

    Swap odd and even bits

--- Chapter09/SwapOddEven/src/main/java/coding/challenge/Bits.java
+++ Chapter09/SwapOddEven/src/main/java/coding/challenge/Bits.java
@@ -8,6 +8,10 @@ public final class Bits {
 
     public static int swap(int n) {
 
+        if (n < 0) {
+            return -1;
+        }
+
         int moveToEvenPositions = (n & 0b10101010101010101010101010101010) >>> 1;
         int moveToOddPositions = (n & 0b1010101010101010101010101010101) << 1;
 
