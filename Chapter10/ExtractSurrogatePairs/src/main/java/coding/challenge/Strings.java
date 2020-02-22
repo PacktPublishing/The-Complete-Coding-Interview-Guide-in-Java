b0754fc088f7802a0a7896bb9fafd8bda45674c3
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-22 15:16:59 +0200

    Extract code points of surrogate pairs

--- Chapter10/ExtractSurrogatePairs/src/main/java/coding/challenge/Strings.java
+++ Chapter10/ExtractSurrogatePairs/src/main/java/coding/challenge/Strings.java
@@ -35,7 +35,7 @@ public final class Strings {
                 result.add(str.codePointAt(i+1));
                 i++;
             } 
-             */
+            */
         }
 
         return result;
