0cb4313d35a16488c6d8d1b709cbfcf9d98b3cde
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-24 18:02:17 +0200

    Finds all pairs whose sum is equal to a given number

---
+++ Chapter10/FindPairsSumEqualK/src/main/java/coding/challenge/Arrays.java
@@ -0,0 +1,43 @@
+package coding.challenge;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class Arrays {
+
+    private Arrays() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static List<String> pairs(int[] m, int k) {
+
+        if (m == null || m.length < 2) {
+            return Collections.emptyList();
+        }
+
+        List<String> result = new ArrayList<>();
+
+        java.util.Arrays.sort(m);
+
+        int l = 0;
+        int r = m.length - 1;
+
+        while (l < r) {
+
+            int sum = m[l] + m[r];
+
+            if (sum == k) {
+                result.add("(" + m[l] + " + " + m[r] + ")");
+                l++;
+                r--;
+            } else if (sum < k) {
+                l++;
+            } else if (sum > k) {
+                r--;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Chapter10/FindPairsSumEqualK/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,16 @@
+package coding.challenge;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sum = 10;
+        int[] arr = new int[]{-5, -2, 5, 4, 3, 7, 2, 1, -1, -2, 15, 6, 12, -4, 3};
+
+        List<String> result = Arrays.pairs(arr, sum);
+
+        System.out.println("Result: " + result);
+    }
+}
