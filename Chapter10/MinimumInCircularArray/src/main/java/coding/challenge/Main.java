2c8398f0b51b5b16342613b48f2735b90afbbf22
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-26 18:06:10 +0200

    Minimum in a circular sorted array

---
+++ Chapter10/MinimumInCircularArray/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,14 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] m = {7, 11, 12, 13, 15, -4, -3, -2, 0, 1, 1, 3, 4, 7, 7};
+
+        int max = Arrays.findMin(m);
+
+        System.out.println("Array: " + java.util.Arrays.toString(m));
+        System.out.println("Max value is: " + max);
+    }
+}
