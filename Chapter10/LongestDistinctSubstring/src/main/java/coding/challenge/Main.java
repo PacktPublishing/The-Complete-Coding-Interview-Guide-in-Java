441d5895aac8310d4cca2d074c0d56a1da347eb4
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-27 09:11:08 +0200

    Longest distinct substring

---
+++ Chapter10/LongestDistinctSubstring/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,15 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str = "abcc#$%^&ccdbeeebdd012333dbsssbesbaas";
+
+        String result = Strings.longestSubstring(str);
+
+        System.out.println("String: " + str);
+        System.out.println("Result: " + result);
+    }
+
+}
---
+++ Chapter10/LongestDistinctSubstring/src/main/java/coding/challenge/Strings.java
@@ -0,0 +1,48 @@
+package coding.challenge;
+
+public final class Strings {
+
+    private Strings() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    private static final int ASCII_CODES = 256;
+
+    public static String longestSubstring(String str) {
+
+        // flag characters present in current window
+        boolean[] flagWindow = new boolean[ASCII_CODES];
+
+        // longest substring boundaries
+        int left = 0;
+        int right = 0;
+
+        // wl and wr represents the sliding window boundaries
+        for (int wl = 0, wr = 0; wr < str.length(); wr++) {
+
+            // character is present in current window
+            if (flagWindow[str.charAt(wr)]) {
+                // remove all characters from the left of the window till the current character
+                while (str.charAt(wl) != str.charAt(wr)) {
+                    flagWindow[str.charAt(wl)] = false;
+                    wl++;
+                }
+
+                // remove current character
+                wl++;
+            } else {
+                // current character is not in the current so we add it
+                flagWindow[str.charAt(wr)] = true;
+
+                // update maximum window size if necessary
+                if ((right - left) < (wr - wl)) {
+                    left = wl;
+                    right = wr;
+                };
+            }
+        }
+
+        // return longest substring
+        return str.substring(left, right + 1);
+    }
+}
