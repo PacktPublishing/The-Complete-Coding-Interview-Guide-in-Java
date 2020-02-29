dbd861630b5effca1ba7faf7458764a9d3b32d2f
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-29 07:51:35 +0200

    Map implementation

---
+++ Chapter11/Map/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,20 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyMap<String, Integer> map = new MyMap<>();
+        
+        map.put("Alex", 1);
+        map.put("Tomy", 2);
+        map.put("Anna", 3); 
+        
+        System.out.println("Alex value is: " + map.get("Alex"));
+        
+        map.remove("Alex");
+
+        System.out.println(map);
+    }
+
+}
---
+++ Chapter11/Map/src/main/java/coding/challenge/MyMap.java
@@ -0,0 +1,119 @@
+package coding.challenge;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class MyMap<K, V> {
+
+    private final class MyEntry<K, V> {
+
+        private final K key;
+        private V value;
+
+        public MyEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + "key=" + key + ", value=" + value + '}';
+        }
+    }
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    private MyEntry<K, V>[] entries = new MyEntry[DEFAULT_CAPACITY];
+
+    public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (entries[i] != null) {
+                if (entries[i].getKey().equals(key)) {
+                    return entries[i].getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void put(K key, V value) {
+
+        boolean inserted = true;
+
+        for (int i = 0; i < size; i++) {
+            if (entries[i].getKey().equals(key)) {
+                entries[i].setValue(value);
+                inserted = false;
+            }
+        }
+
+        if (inserted) {
+            checkCapacity();
+            entries[size++] = new MyEntry<>(key, value);
+        }
+    }
+
+    private void checkCapacity() {
+        if (size == entries.length) {
+            int newSize = entries.length * 2;
+            entries = Arrays.copyOf(entries, newSize);
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void remove(K key) {
+
+        for (int i = 0; i < size; i++) {
+            if (entries[i].getKey().equals(key)) {
+                entries[i] = null;
+
+                size--;
+
+                condenseArray(i);
+            }
+        }
+    }
+
+    private void condenseArray(int start) {
+
+        int i;
+        for (i = start; i < size; i++) {
+            entries[i] = entries[i + 1];
+        }
+
+        entries[i] = null;
+    }
+
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            set.add(entries[i].getKey());
+        }
+        return set;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(entries);
+    }
+}
