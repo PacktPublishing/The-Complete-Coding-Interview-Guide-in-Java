3aa2851545420596f05f65e977605a1b15f0ed4b
AnghelLeonard <leoprivacy@yahoo.com>
2020-02-29 17:49:43 +0200

    Split at n index

---
+++ Chapter11/LinkedListSplitInTwo/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,32 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {        
+
+        SinglyLinkedList sll = new SinglyLinkedList();
+
+        sll.insertFirst(11);
+        sll.insertFirst(10);
+        sll.insertFirst(9);
+        sll.insertFirst(8);
+        sll.insertFirst(8);
+        sll.insertFirst(8);
+        sll.insertFirst(7);
+        sll.insertFirst(6);
+        sll.insertFirst(5);
+        sll.insertFirst(4);
+        sll.insertFirst(-3);
+        sll.insertFirst(2);
+        sll.insertFirst(1);
+        sll.insertFirst(11);
+        sll.insertFirst(90);
+
+        sll.print();    
+        
+        sll.split(7);
+        
+        sll.print();
+    }
+    
+}
---
+++ Chapter11/LinkedListSplitInTwo/src/main/java/coding/challenge/SinglyLinkedList.java
@@ -0,0 +1,73 @@
+package coding.challenge;
+
+public final class SinglyLinkedList {
+
+    private final class Node {
+
+        private int data;
+        private Node next;
+
+        @Override
+        public String toString() {
+            return " " + data + " ";
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    public void insertFirst(int data) {
+
+        Node newNode = new Node();
+
+        newNode.data = data;
+        newNode.next = head;
+        head = newNode;
+
+        if (tail == null) {
+            tail = newNode;
+        }
+    }
+
+    public void split(int n) {
+
+        Node currentNode = head;
+        head = currentNode;
+        tail = currentNode;
+
+        while (currentNode != null) {
+            
+            Node nextNode = currentNode.next;
+            
+            if (currentNode.data < n) {
+                // insert node at the head
+                currentNode.next = head;
+                head = currentNode;
+            } else {
+                // insert node at the tail
+                tail.next = currentNode;
+                tail = currentNode;
+            }
+            
+            currentNode = nextNode;
+        }
+        
+        tail.next = null;
+    }
+
+    public void print() {
+
+        System.out.println("Head (" + head + ") ----------> Last (" + tail + "):");
+
+        Node currentNode = head;
+        while (currentNode != null) {
+
+            System.out.print(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+
+}
