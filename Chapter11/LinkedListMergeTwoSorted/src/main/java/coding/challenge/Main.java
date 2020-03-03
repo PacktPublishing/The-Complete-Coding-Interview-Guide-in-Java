32f0459545f933ac0c533ffbc7e3fff66af495b6
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-03 16:35:19 +0200

    Merge two sorted linked list no extra space

--- Chapter11/LinkedListMergeTwoSorted/src/main/java/coding/challenge/Main.java
+++ Chapter11/LinkedListMergeTwoSorted/src/main/java/coding/challenge/Main.java
@@ -8,21 +8,14 @@ public class Main {
 
         sll1.insertFirst(10);
         sll1.insertFirst(8);
-        sll1.insertFirst(7);
-        sll1.insertFirst(4);
+        sll1.insertFirst(7);        
         sll1.insertFirst(4);
         
         SinglyLinkedList sll2 = new SinglyLinkedList();
-
-        sll2.insertFirst(22);
-        sll2.insertFirst(18);
-        sll2.insertFirst(17);
-        sll2.insertFirst(16);
-        sll2.insertFirst(15);
+        
         sll2.insertFirst(11);
-        sll2.insertFirst(9);
+        sll2.insertFirst(9);        
         sll2.insertFirst(5);
-        sll2.insertFirst(3);
         
         sll1.print();
         sll2.print();
--- Chapter11/LinkedListMergeTwoSorted/src/main/java/coding/challenge/SinglyLinkedList.java
+++ Chapter11/LinkedListMergeTwoSorted/src/main/java/coding/challenge/SinglyLinkedList.java
@@ -30,7 +30,7 @@ public final class SinglyLinkedList {
         Node list1 = head;      // this is the merged linked list (the final result)
         Node list2 = sll.head;  // from this list we add nodes at appropriate place in list1
 
-        // if the merged list head bigger then we need to swap the heads
+        // if the head of the merged list is bigger than the head of list2 then we need to swap the heads
         if (list1.data < list2.data) {
             head = list1;
         } else {
@@ -47,10 +47,12 @@ public final class SinglyLinkedList {
                 list1.next = list2;
                 list2 = auxNode;
             }
-                        
+        
+            // advance to the last node in the merged linked list
             list1 = list1.next;
         }
 
+        // add the remaining list2
         if (list1.next == null) {
             list1.next = list2;
         }
