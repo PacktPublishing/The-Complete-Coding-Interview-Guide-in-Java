0e363038d6942612642d970992fc596ea58fc7d4
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-03 16:46:31 +0200

    Merge two sorted linked list via recursion

---
+++ Chapter11/LinkedListMergeTwoSortedRecursion/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,30 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {        
+
+        SinglyLinkedList sll1 = new SinglyLinkedList();
+
+        sll1.insertFirst(10);
+        sll1.insertFirst(8);
+        sll1.insertFirst(7);        
+        sll1.insertFirst(4);
+        
+        SinglyLinkedList sll2 = new SinglyLinkedList();
+        
+        sll2.insertFirst(11);
+        sll2.insertFirst(9);        
+        sll2.insertFirst(5);
+        
+        sll1.print();
+        sll2.print();
+        
+        sll1.merge(sll2);
+        
+        System.out.println("\nAfter merging:");
+
+        sll1.print();                       
+    }
+
+}
