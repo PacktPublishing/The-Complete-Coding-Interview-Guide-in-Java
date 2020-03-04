537abb6e78df74192926927df1a13f8af518cd50
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-04 13:31:07 +0200

    Reverse a doubly linked list

---
+++ Chapter11/DoublyLinkedListReverse/src/main/java/coding/challenge/DoublyLinkedList.java
@@ -0,0 +1,71 @@
+package coding.challenge;
+
+public final class DoublyLinkedList {
+
+    private final class Node {
+
+        private int data;
+        private Node next;
+        private Node prev;
+
+        @Override
+        public String toString() {
+            return " " + data + " ";
+        }
+    }
+
+    private Node head;
+
+    public void insertFirst(int data) {
+
+        Node newNode = new Node();
+
+        newNode.data = data;
+        newNode.next = head;
+
+        if (head != null) {
+            head.prev = newNode;
+        }
+
+        head = newNode;
+    }
+
+    public void reverse() {
+
+        Node currentNode = head;
+        Node prevNode = null;
+
+        while (currentNode != null) {
+
+            // swap next and prev pointers of the current node
+            Node prev = currentNode.prev;
+            currentNode.prev = currentNode.next;
+            currentNode.next = prev;
+
+            // update the previous node before moving to the next node
+            prevNode = currentNode;
+
+            // move to the next node in the doubly linked list            
+            currentNode = currentNode.prev;
+        }
+
+        // update the head to point to the last node
+        if (prevNode != null) {
+            head = prevNode;
+        }
+    }
+
+    public void print() {
+
+        System.out.println("\nHead (" + head + ") ----------> Last:");
+
+        Node currentNode = head;
+        while (currentNode != null) {
+
+            System.out.print(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        System.out.println();
+    }
+}
---
+++ Chapter11/DoublyLinkedListReverse/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,26 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList dll = new DoublyLinkedList();
+
+        dll.insertFirst(122);
+        dll.insertFirst(4);
+        dll.insertFirst(7);
+        dll.insertFirst(-2);
+        dll.insertFirst(2);
+        dll.insertFirst(21);
+        dll.insertFirst(22);
+
+        System.out.println("\nInitial:");
+        dll.print();
+
+        dll.reverse();
+
+        System.out.println("\nReversed:");
+        dll.print();
+    }
+
+}
