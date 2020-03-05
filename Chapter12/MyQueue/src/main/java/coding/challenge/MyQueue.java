5e07542521ce68071b44a5e4a824476e62af0b32
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-05 11:32:12 +0200

    Queue implementation

--- Chapter12/MyQueue/src/main/java/coding/challenge/MyQueue.java
+++ Chapter12/MyQueue/src/main/java/coding/challenge/MyQueue.java
@@ -8,26 +8,27 @@ public final class MyQueue<E> {
 
     private static final int DEFAULT_CAPACITY = 10;
 
-    private int front;
-    private int rear;
-    private int count;
-    private int capacity;
+    private int front;    // elements are removed or peeked from the front
+    private int rear;     // elements are added in the rear
+    private int count;    // size of the queue
+    private int capacity; // capacity of the queue (this is doubled when is exceeded)
 
-    private E[] queue;
+    private E[] queue;    // the array that sits behind the queue
 
     // constructor to initialize the queue
     MyQueue() {
+        // we use Java Reflection since Java doesn't allow us to instantiate a generic array
         queue = (E[]) Array.newInstance(
                 Object[].class.getComponentType(), DEFAULT_CAPACITY);
 
-        count = 0; // queue size
-        front = 0; // front of the queue 
-        rear = -1; // back of the queue
+        count = 0; // initially, the size of the queue is 0
+        front = 0; // the index of the first element is 0
+        rear = -1; // initially, there is no element in the queue
 
-        capacity = DEFAULT_CAPACITY;
+        capacity = DEFAULT_CAPACITY; // initially, the capacity is of 10 elements
     }
 
-    // add an element e in the queue
+    // add an element 'e' in the queue
     public void enqueue(E e) {
 
         // if the queue is full, we double its capacity
@@ -35,31 +36,43 @@ public final class MyQueue<E> {
             ensureCapacity();
         }
 
+        // adding the element in the rear of the queue
         rear = (rear + 1) % capacity;
         queue[rear] = e;
+        
+        // update the size of the queue
         count++;
     }
 
     // remove and return the front element from the queue
     public E dequeue() {
 
+        // if the queue is empty we just throw a meaningful exception
         if (isEmpty()) {
             throw new EmptyStackException();
         }
 
+        // extract the element from the front
         E e = queue[front];
+        
+        // set the new front
         front = (front + 1) % capacity;
+        
+        // decrease the size of the queue
         count--;
-
+       
         return e;
     }
 
     // return but not remove the front element in the queue
     public E peek() {
+        
+        // if the queue is not empty then extract the element from the front
         if (!isEmpty()) {
             return queue[front];
         }
 
+        // the queue is empty, therefore throw a meaningful exception
         throw new EmptyStackException();
     }
 
@@ -78,9 +91,12 @@ public final class MyQueue<E> {
         return size() == capacity;
     }
 
+    // used internally for doubling the queue capacity
     private void ensureCapacity() {       
         int newSize = queue.length * 2;
         queue = Arrays.copyOf(queue, newSize);
+        
+        // setting the new capacity
         capacity = newSize;
     }
 }
