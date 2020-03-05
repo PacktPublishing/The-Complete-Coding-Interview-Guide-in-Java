4d5383b6e3000a3a72b163ec0bc0b6011e13d379
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-05 16:05:56 +0200

    Min stack in O(n) space

---
+++ Chapter12/MinStack/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,27 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyStack stack = new MyStack();
+
+        stack.push(25);
+        stack.push(35);
+        stack.push(15);
+
+        System.out.println("Min: " + stack.min());
+        
+        stack.push(-1);
+        stack.push(-3);
+        stack.push(6);
+        
+        System.out.println("Min: " + stack.min());
+        
+        stack.pop();
+        stack.pop();
+        
+        System.out.println("Min: " + stack.min());        
+    }
+
+}
---
+++ Chapter12/MinStack/src/main/java/coding/challenge/MyStack.java
@@ -0,0 +1,41 @@
+package coding.challenge;
+
+import java.util.Stack;
+
+public class MyStack extends Stack<Integer> {
+
+    Stack<Integer> stackOfMin;
+
+    public MyStack() {
+        stackOfMin = new Stack<>();
+    }
+  
+    public Integer push(int value) {
+        
+        if (value <= min()) {
+            stackOfMin.push(value);
+        }
+        
+        return super.push(value);
+    }
+
+    @Override
+    public Integer pop() {
+        
+        int value = super.pop();
+        if (value == min()) {
+            stackOfMin.pop();
+        }
+        
+        return value;
+    }
+
+    public int min() {
+        
+        if (stackOfMin.isEmpty()) {
+            return Integer.MAX_VALUE;
+        } else {
+            return stackOfMin.peek();
+        }
+    }
+}
