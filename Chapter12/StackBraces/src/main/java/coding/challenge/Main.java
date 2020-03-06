9e36546da1814d16f4a853b0e72b891173aea1f0
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-06 12:15:28 +0200

    Matching curly braces

--- Chapter12/StackBraces/src/main/java/coding/challenge/Main.java
+++ Chapter12/StackBraces/src/main/java/coding/challenge/Main.java
@@ -1,15 +1,16 @@
 package coding.challenge;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        int stockPrices[] = {55, 34, 22, 23, 27, 88, 9, 6, 5, 65};
-        int[] spanResult = StockSpan.stockSpan(stockPrices);
+        String goodBracesStr = "{}{{{}}}{{}}{}";
+        String wrongBracesStr = "{}{{{}}}{{}{{}";
 
-        System.out.println("Stock prices: " + Arrays.toString(stockPrices));
-        System.out.println("Span results: " + Arrays.toString(spanResult));
+        System.out.println("Good (should return true): " 
+                + StackBraces.bracesMatching(goodBracesStr));
+        
+        System.out.println("Wrong (should return false): " 
+                + StackBraces.bracesMatching(wrongBracesStr));
     }
 }
