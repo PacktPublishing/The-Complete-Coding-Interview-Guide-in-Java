023166e517699e675581c381f62c9a051ad3f15e
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-06 08:45:15 +0200

    Stack of plates

---
+++ Chapter12/StackOfPlates/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,29 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyStack stack = new MyStack();
+
+        stack.push(25);
+        stack.push(35);
+        stack.push(15);
+        stack.push(-1);
+        stack.push(-3);
+        stack.push(6);
+        stack.push(2);
+        stack.push(12);
+
+        stack.printStacks();
+        
+        System.out.println("\n\nPop from stack 1: " + stack.popAt(1));
+        
+        stack.printStacks();
+        
+        System.out.println("\n\nPop from stack 0: " + stack.popAt(0));
+        
+        stack.printStacks();
+    }
+
+}
---
+++ Chapter12/StackOfPlates/src/main/java/coding/challenge/MyStack.java
@@ -0,0 +1,96 @@
+package coding.challenge;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class MyStack {
+
+    private static final int STACK_SIZE = 3;
+
+    private final LinkedList<Stack<Integer>> stacks = new LinkedList<>();
+
+    public void push(int value) {
+
+        // if there is no stack or the last stack is full
+        if (stacks.isEmpty() || stacks.getLast().size() >= STACK_SIZE) {
+
+            // create a new stack and push the value into it
+            Stack<Integer> stack = new Stack<>();
+            stack.push(value);
+
+            // add the new stack into the list of stacks
+            stacks.add(stack);
+        } else {
+            // add the value in the last stack
+            stacks.getLast().push(value);
+        }
+    }
+
+    public Integer pop() {
+
+        if (stacks.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        // find the last stack
+        Stack<Integer> lastStack = stacks.getLast();
+
+        // pop the value from the last stack
+        int value = lastStack.pop();
+
+        // if last stack is empty we can remove it from the list of stacks
+        removeStackIfEmpty();
+
+        return value;
+    }
+
+    public Integer popAt(int index) {
+
+        if (stacks.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        if (index < 0 || index >= stacks.size()) {
+            throw new IllegalArgumentException("Th given index is out of bounds");
+        }
+
+        // get the value from the correspondind stack
+        int value = stacks.get(index).pop();
+
+        // since we pop an element we must shift the remaining elements        
+        shift(index);
+
+        // if last stack is empty we can remove it from the list of stacks
+        removeStackIfEmpty();
+
+        return value;
+    }
+
+    private void shift(int index) {
+
+        for (int i = index; i < stacks.size() - 1; ++i) {
+            Stack<Integer> currentStack = stacks.get(i);
+            Stack<Integer> nextStack = stacks.get(i + 1);
+
+            currentStack.push(nextStack.remove(0));
+        }
+    }
+
+    private void removeStackIfEmpty() {
+        if (stacks.getLast().isEmpty()) {
+            stacks.removeLast();
+        }
+    }
+
+    public void printStacks() {
+        for (int i = 0; i < stacks.size(); i++) {
+            System.out.println("\nStack " + (i) + ": ");
+            System.out.print("BOTTOM -> ");
+            for (int value : stacks.get(i)) {
+                System.out.print(value + " ");
+            }
+            System.out.print(" <- TOP\n");
+        }
+    }
+}
