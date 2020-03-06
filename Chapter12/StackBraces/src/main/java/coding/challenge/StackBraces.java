7a1b6275194142c19fc35ffb13d0fb1c0e2f3ea7
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-06 12:10:08 +0200

    Matching curly braces

---
+++ Chapter12/StackBraces/src/main/java/coding/challenge/StackBraces.java
@@ -0,0 +1,42 @@
+package coding.challenge;
+
+import java.util.Stack;
+
+public final class StackBraces {
+
+    private StackBraces() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static boolean bracesMatching(String bracesStr) {
+
+        if (bracesStr == null || bracesStr.isBlank()) {
+            return false;
+        }
+
+        Stack<Character> stackBraces = new Stack<>();
+
+        int len = bracesStr.length();
+        for (int i = 0; i < len; i++) {
+            
+            switch (bracesStr.charAt(i)) {
+                case '{':
+                    stackBraces.push(bracesStr.charAt(i));
+                    break;
+
+                case '}':
+                    if (stackBraces.isEmpty()) { // we find a mismatch
+                        return false; 
+                    }
+                    
+                    stackBraces.pop(); // for every match we pop the corresponding '{'
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+        
+        return stackBraces.empty();
+    }
+}
