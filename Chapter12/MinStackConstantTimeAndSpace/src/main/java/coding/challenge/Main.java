d7c626730d2001db2231001518e9e57308d75b3d
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-07 08:20:01 +0200

    Min stack in O(1) time and space

---
+++ Chapter12/MinStackConstantTimeAndSpace/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,40 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyStack stack = new MyStack();
+
+        stack.push(5);
+        stack.push(-25);
+        stack.push(35);
+        stack.push(15);
+        System.out.println("Min: " + stack.min());
+
+        stack.push(-1);
+        stack.push(-30);
+        stack.push(6);
+        System.out.println("Min: " + stack.min());
+
+        stack.push(-100);
+        System.out.println("Min: " + stack.min());
+
+        stack.pop();
+        stack.pop();
+        System.out.println("Min (after two pop): " + stack.min());
+
+        stack.pop();
+        stack.pop();
+        System.out.println("Min (after two pop): " + stack.min());
+
+        stack.pop();
+        stack.pop();
+        stack.pop();        
+        System.out.println("Min (after three pop): " + stack.min());
+        
+        stack.pop();     
+        System.out.println("Min (after two pop): " + stack.min());
+    }
+
+}
---
+++ Chapter12/MinStackConstantTimeAndSpace/src/main/java/coding/challenge/MyStack.java
@@ -0,0 +1,45 @@
+package coding.challenge;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class MyStack {
+
+    private int min;
+    private final Stack<Integer> stack = new Stack<>();
+
+    public void push(int value) {
+
+        // we don't allow values that overflow int/2 range
+        int r = Math.addExact(value, value);
+
+        if (stack.empty()) {
+            stack.push(value);
+            min = value;
+        } else if (value > min) {
+            stack.push(value);
+        } else {
+            stack.push(r - min);
+            min = value;
+        }
+    }
+
+    // pop() doesn't return the value since this 
+    // may be a wrong value (a value that was not pushed by the client)!
+    public void pop() {
+
+        if (stack.empty()) {
+            throw new EmptyStackException();
+        }
+
+        int top = stack.peek();
+        if (top < min) {
+            min = 2 * min - top;
+        }
+        stack.pop();
+    }
+
+    public int min() {
+        return min;
+    }
+}
