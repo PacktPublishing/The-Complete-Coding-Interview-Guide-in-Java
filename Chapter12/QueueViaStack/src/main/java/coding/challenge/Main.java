bd16b5ac5a28b09c5dd1f56b7baf9fccc3ae9c5a
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-07 09:32:27 +0200

    Queue via stack

---
+++ Chapter12/QueueViaStack/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,15 @@
+package coding.challenge;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int stockPrices[] = {55, 34, 22, 23, 27, 88, 70, 42, 51, 100};
+        int[] spanResult = StockSpan.stockSpan(stockPrices);
+
+        System.out.println("Stock prices: " + Arrays.toString(stockPrices));
+        System.out.println("Span results: " + Arrays.toString(spanResult));
+    }
+}
