518def0e757c52f775dc3b327567f7173e7c3c01
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-07 18:15:08 +0200

    Sort stack

---
+++ Chapter12/SortStack/src/main/java/coding/challenge/Stacks.java
@@ -0,0 +1,29 @@
+package coding.challenge;
+
+import java.util.Stack;
+
+public final class Stacks {
+
+    private Stacks() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static void sort(Stack<Integer> stack) {
+
+        Stack<Integer> auxStack = new Stack<>();
+
+        while (!stack.isEmpty()) {
+
+            int t = stack.pop();
+            while (!auxStack.isEmpty() && auxStack.peek() > t) {
+                stack.push(auxStack.pop());
+            }
+            auxStack.push(t);
+        }
+
+        // copy the elements from 'auxStack' back into 'stack'
+        while (!auxStack.isEmpty()) {
+            stack.push(auxStack.pop());
+        }
+    }
+}
