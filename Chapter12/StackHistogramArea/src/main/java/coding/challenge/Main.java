1210b2f97fe9b3b5719783ea4de6543813f6b6c6
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-08 12:00:52 +0200

    Histogram area

---
+++ Chapter12/StackHistogramArea/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,13 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] histogram = {4, 1, 8, 6, 5, 3};
+        
+        int ma = Stacks.maxAreaUsingStack(histogram);
+        
+        System.out.println("Max area: " + ma);
+    }
+}
---
+++ Chapter12/StackHistogramArea/src/main/java/coding/challenge/Stacks.java
@@ -0,0 +1,47 @@
+package coding.challenge;
+
+import java.util.Stack;
+
+public final class Stacks {
+
+    private Stacks() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int maxAreaUsingStack(int[] histogram) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int maxArea = 0;
+        for (int bar = 0; bar <= histogram.length; bar++) {
+
+            int barHeight;
+            if (bar == histogram.length) {
+                barHeight = 0; // take into account last bar
+            } else {
+                barHeight = histogram[bar];
+            }
+            
+            while (!stack.empty() && barHeight < histogram[stack.peek()]) {
+                                
+                // we found a bar smaller than the one from the stack
+                int top = stack.pop();
+
+                // find left boundary
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                
+                // find the width of the rectangle 
+                int width = bar - left - 1;
+
+                // compute area of the current rectangle
+                int area = width * histogram[top];
+                maxArea = Integer.max(area, maxArea);
+            }
+
+            // add current bar (index) into the stack
+            stack.push(bar);
+        }
+
+        return maxArea;
+    }
+}
