7d8d5c812954d0bdc625257b010178bec92e7c60
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-08 17:36:15 +0200

    Islands

---
+++ Chapter12/QueueIslands/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,25 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] matrix
+                = {
+                    {1, 1, 1, 0, 0, 0, 1, 1, 0, 1},
+                    {0, 1, 1, 0, 1, 0, 1, 0, 0, 0},
+                    {1, 1, 1, 1, 0, 0, 1, 0, 1, 0},
+                    {1, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+                    {1, 1, 1, 1, 1, 1, 0, 0, 0, 1},
+                    {0, 0, 0, 1, 0, 0, 1, 1, 0, 1},
+                    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                    {1, 1, 0, 1, 0, 0, 0, 1, 1, 0},
+                    {1, 0, 0, 0, 1, 1, 0, 1, 0, 0},
+                    {1, 1, 0, 1, 0, 1, 0, 1, 1, 1}
+                };
+
+        int islandsNo = Queues.islands(matrix);
+        System.out.println("Number of islands: " + islandsNo);
+    }
+}
+
---
+++ Chapter12/QueueIslands/src/main/java/coding/challenge/Queues.java
@@ -0,0 +1,85 @@
+package coding.challenge;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public final class Queues {
+
+    private Queues() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    private static class Cell {
+
+        int r, c;
+
+        public Cell(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    // there are 8 possible movements from a cell    
+    private static final int POSSIBLE_MOVEMENTS = 8;
+
+    // top, right, bottom, left and 4 diagonal moves
+    private static final int[] ROW = {-1, -1, -1, 0, 1, 0, 1, 1};
+    private static final int[] COL = {-1, 1, 0, -1, -1, 1, 0, 1};
+
+    public static int islands(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // stores if a cell is flagged or not
+        boolean[][] flagged = new boolean[m][n];
+
+        int island = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == 1 && !flagged[i][j]) {
+                    resolve(matrix, flagged, i, j);
+                    island++;
+                }
+            }
+        }
+
+        return island;
+    }
+
+    private static void resolve(int[][] matrix, boolean[][] flagged, int i, int j) {
+
+        Queue<Cell> queue = new ArrayDeque<>();
+        queue.add(new Cell(i, j));
+
+        // flag source node
+        flagged[i][j] = true;
+
+        while (!queue.isEmpty()) {
+
+            int r = queue.peek().r;
+            int c = queue.peek().c;
+            queue.poll();
+
+            // check for all 8 possible movements from current cell and enqueue each valid movement
+            for (int k = 0; k < POSSIBLE_MOVEMENTS; k++) {
+
+                // skip if location is invalid or already flagged or has water
+                if (isValid(matrix, r + ROW[k], c + COL[k], flagged)) {
+                    flagged[r + ROW[k]][c + COL[k]] = true;
+                    queue.add(new Cell(r + ROW[k], c + COL[k]));
+                }
+            }
+        }
+    }
+
+    // check if it is valid to go to cell (r, c) from current position
+    private static boolean isValid(int[][] matrix, int r, int c,
+            boolean[][] processed) {
+
+        return (r >= 0) && (r < processed.length)
+                && (c >= 0) && (c < processed[0].length)
+                && (matrix[r][c] == 1 && !processed[r][c]);
+    }
+}
