376b4c03c5732b047750040a9672df7424f540f5
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-08 15:24:05 +0200

    Histogram area

--- Chapter12/StackHistogramArea/src/main/java/coding/challenge/Stacks.java
+++ Chapter12/StackHistogramArea/src/main/java/coding/challenge/Stacks.java
@@ -24,24 +24,24 @@ public final class Stacks {
             
             while (!stack.empty() && barHeight < histogram[stack.peek()]) {
                                 
-                // we found a bar smaller than the one from the stack
+                // we found a bar smaller than the one from the stack                
                 int top = stack.pop();
 
                 // find left boundary
                 int left = stack.isEmpty() ? -1 : stack.peek();
                 
-                // find the width of the rectangle 
-                int width = bar - left - 1;
-
+                // find the width of the rectangular area 
+                int areaRectWidth = bar - left - 1;
+                
                 // compute area of the current rectangle
-                int area = width * histogram[top];
+                int area = areaRectWidth * histogram[top];
                 maxArea = Integer.max(area, maxArea);
             }
 
             // add current bar (index) into the stack
             stack.push(bar);
-        }
-
+        }        
+        
         return maxArea;
     }
 }
