03bc833b10ffe55a9b97033c6209c6174f4d0188
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-11 08:47:03 +0200

    Binary tree traversal

--- Chapter13/BinaryTreeTraversal/src/main/java/coding/challenge/BinaryTree.java
+++ Chapter13/BinaryTreeTraversal/src/main/java/coding/challenge/BinaryTree.java
@@ -3,9 +3,7 @@ package coding.challenge;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.ConcurrentModificationException;
-import java.util.Deque;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -16,9 +14,16 @@ public class BinaryTree<T extends Comparable<T>> {
 
     private class Node {
 
-        private T element;
         private Node left;
         private Node right;
+
+        private final T element;
+
+        public Node(T element) {
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
 
         public Node(Node left, Node right, T element) {
             this.element = element;
@@ -34,112 +39,49 @@ public class BinaryTree<T extends Comparable<T>> {
         LEVEL
     }
 
-    public boolean add(T element) {
+    public boolean insert(T element) {
 
         if (element == null) {
             return false;
         }
 
-        if (contains(element)) {
-            return false;
+        if (root == null) {
+            root = new Node(element);
+            nodeCount++;
+
+            return true;
         }
 
-        root = addRecursive(root, element);
-        nodeCount++;
+        insert(root, element);
 
         return true;
     }
 
-    private Node addRecursive(Node current, T element) {
+    private void insert(Node node, T element) {
 
-        if (current == null) {
-            return new Node(null, null, element);
-        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
 
-        if (element.compareTo(current.element) < 0) {
-            current.left = addRecursive(current.left, element);
-        } else {
-            current.right = addRecursive(current.right, element);
-        }
+        while (!queue.isEmpty()) {
+            node = queue.peek();
+            queue.remove();
 
-        return current;
-    }
-
-    public boolean contains(T element) {
-
-        return containsNodeRecursive(root, element);
-    }
-
-    private boolean containsNodeRecursive(Node current, T element) {
-
-        if (current == null || element == null) {
-            return false;
-        }
-
-        if (element.compareTo(current.element) == 0) {
-            return true;
-        }
-
-        return element.compareTo(current.element) < 0
-                ? containsNodeRecursive(current.left, element)
-                : containsNodeRecursive(current.right, element);
-    }
-
-    public boolean delete(T element) {
-
-        if (contains(element)) {
-            root = deleteRecursive(root, element);
-            nodeCount--;
-
-            return true;
-        }
-
-        return false;
-    }
-
-    private Node deleteRecursive(Node node, T element) {
-
-        if (node == null) {
-            return null;
-        }
-
-        if (element.compareTo(node.element) < 0) {
-            node.left = deleteRecursive(node.left, element);
-        } else if (element.compareTo(node.element) > 0) {
-            node.right = deleteRecursive(node.right, element);
-        }
-
-        if (element.compareTo(node.element) == 0) {
-
-            if (node.right == null) {
-                Node left = node.left;
-
-                node = null;
-                return left;
-            } else if (node.left == null) {
-                Node right = node.right;
-
-                node = null;
-                return right;
+            if (node.left == null) {
+                node.left = new Node(element);
+                nodeCount++;
+                break;
             } else {
-                Node leftmost = findLeftmostNode(node.right);
-
-                node.element = leftmost.element;
-                node.right = deleteRecursive(node.right, node.element);
+                queue.add(node.left);
             }
 
+            if (node.right == null) {
+                node.right = new Node(element);
+                nodeCount++;
+                break;
+            } else {
+                queue.add(node.right);
+            }
         }
-
-        return node;
-    }
-
-    public T min() {
-
-        if (root == null) {
-            return null;
-        }
-
-        return findLeftmostNode(root).element;
     }
 
     public T root() {
@@ -151,48 +93,8 @@ public class BinaryTree<T extends Comparable<T>> {
         return root.element;
     }
 
-    public T max() {
-
-        if (root == null) {
-            return null;
-        }
-
-        return findRightmostNode(root).element;
-    }
-
-    private Node findLeftmostNode(Node node) {
-
-        while (node.left != null) {
-            node = node.left;
-        }
-
-        return node;
-    }
-
-    private Node findRightmostNode(Node node) {
-
-        while (node.right != null) {
-            node = node.right;
-        }
-
-        return node;
-    }
-
     public int size() {
         return nodeCount;
-    }
-
-    public int height() {
-        return height(root);
-    }
-
-    private int height(Node node) {
-
-        if (node == null) {
-            return 0;
-        }
-
-        return Math.max(height(node.left), height(node.right)) + 1;
     }
 
     public void print(TraversalOrder to) {
@@ -201,7 +103,7 @@ public class BinaryTree<T extends Comparable<T>> {
             System.out.println("empty");
             return;
         }
-        
+
         switch (to) {
             case IN:
                 printInOrder(root);
@@ -320,7 +222,7 @@ public class BinaryTree<T extends Comparable<T>> {
 
     private void traverseLevelOrderAsList(Node node, List<T> treeList) {
 
-        Queue<Node> queue = new ArrayDeque<>();
+        Queue<Node> queue = new LinkedList<>();
 
         queue.add(node);
 
@@ -340,214 +242,4 @@ public class BinaryTree<T extends Comparable<T>> {
         }
     }
 
-    public Iterator<T> iterator(TraversalOrder to) {
-
-        if (size() == 0) {
-            return Collections.emptyIterator();
-        }
-
-        switch (to) {
-            case IN:
-                return traverseInOrderAsIterator(root);
-            case PRE:
-                return traversePreOrderAsIterator(root);
-            case POST:
-                return traversePostOrderAsIterator(root);
-            case LEVEL:
-                return traverseLevelOrderAsIterator(root);
-            default:
-                throw new IllegalArgumentException("Unrecognized traversal order");
-        }
-    }
-
-    private Iterator<T> traverseInOrderAsIterator(Node node) {
-
-        final int expectedNodeCount = size();
-
-        final Deque<Node> stack = new ArrayDeque<>();
-
-        stack.push(node);
-
-        return new Iterator<T>() {
-            Node cursor = node;
-
-            @Override
-            public boolean hasNext() {
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                return node != null && !stack.isEmpty();
-            }
-
-            @Override
-            public T next() {
-
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                while (cursor != null && cursor.left != null) {
-                    stack.push(cursor.left);
-                    cursor = cursor.left;
-                }
-
-                Node current = stack.pop();
-
-                if (current.right != null) {
-                    stack.push(current.right);
-                    cursor = current.right;
-                }
-
-                return current.element;
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
-
-    private Iterator<T> traversePreOrderAsIterator(Node node) {
-
-        final int expectedNodeCount = size();
-
-        final Deque<Node> stack = new ArrayDeque<>();
-
-        stack.push(node);
-
-        return new Iterator<T>() {
-
-            @Override
-            public boolean hasNext() {
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                return node != null && !stack.isEmpty();
-            }
-
-            @Override
-            public T next() {
-
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                Node current = stack.pop();
-
-                if (current.right != null) {
-                    stack.push(current.right);
-                }
-
-                if (current.left != null) {
-                    stack.push(current.left);
-                }
-
-                return current.element;
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
-
-    private Iterator<T> traversePostOrderAsIterator(Node node) {
-
-        final int expectedNodeCount = size();
-
-        final Deque<Node> stacka = new ArrayDeque<>();
-        final Deque<Node> stackb = new ArrayDeque<>();
-
-        stacka.push(node);
-
-        while (!stacka.isEmpty()) {
-            Node nodea = stacka.pop();
-            if (nodea != null) {
-                stackb.push(nodea);
-                if (nodea.left != null) {
-                    stacka.push(nodea.left);
-                }
-                if (nodea.right != null) {
-                    stacka.push(nodea.right);
-                }
-            }
-        }
-
-        return new Iterator<T>() {
-
-            @Override
-            public boolean hasNext() {
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                return node != null && !stackb.isEmpty();
-            }
-
-            @Override
-            public T next() {
-
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                return stackb.pop().element;
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
-
-    private Iterator<T> traverseLevelOrderAsIterator(Node node) {
-
-        final int expectedNodeCount = size();
-
-        final Queue<Node> queue = new ArrayDeque<>();
-
-        queue.offer(node);
-
-        return new Iterator<T>() {
-
-            @Override
-            public boolean hasNext() {
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                return node != null && !queue.isEmpty();
-            }
-
-            @Override
-            public T next() {
-
-                if (expectedNodeCount != nodeCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                Node current = queue.poll();
-
-                if (current.left != null) {
-                    queue.offer(current.left);
-                }
-
-                if (current.right != null) {
-                    queue.offer(current.right);
-                }
-
-                return current.element;
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
 }
--- Chapter13/BinaryTreeTraversal/src/main/java/coding/challenge/Main.java
+++ Chapter13/BinaryTreeTraversal/src/main/java/coding/challenge/Main.java
@@ -7,32 +7,22 @@ public class Main {
     /*        
               40
             /    \
-          30      45
-         /  \    /  \
-        15  33  42  56
-       / \      /     \    
-      5  17    41      67
-        
+          47      45
+         /  \    /  
+        11   3  44         
     */
 
         BinaryTree<Integer> bt = new BinaryTree<>();
         
-        bt.add(40);
-        bt.add(30);
-        bt.add(45);
-        bt.add(15);
-        bt.add(33);
-        bt.add(42);
-        bt.add(56);
-        bt.add(5);
-        bt.add(17);
-        bt.add(41);
-        bt.add(67);             
-                  
-        System.out.println("Root: " + bt.root());
-        System.out.println("Root Height: " + bt.height());
-        System.out.println("Min: " + bt.min());
-        System.out.println("Max: " + bt.max());
+        bt.insert(40);
+        bt.insert(47);
+        bt.insert(45);
+        bt.insert(11);
+        bt.insert(3);
+        bt.insert(44);
+          
+        System.out.println("Root: " + bt.root() + " Size: " + bt.size());
+      
         
         System.out.println("\n\nTraversal LEVEL: ");
         bt.print(BinaryTree.TraversalOrder.LEVEL);
@@ -45,7 +35,7 @@ public class Main {
         
         System.out.println("\n\nTraversal POST-ORDER: ");
         bt.print(BinaryTree.TraversalOrder.POST);        
-        
+        /*
         System.out.println("\n\nDeleting 33, 45, 67 and 5\n\n");
         bt.delete(33);        
         bt.delete(45);
@@ -68,6 +58,7 @@ public class Main {
         
         System.out.println("\n\nTraversal POST-ORDER: ");
         bt.print(BinaryTree.TraversalOrder.POST);                
+*/
     }
 
 }
