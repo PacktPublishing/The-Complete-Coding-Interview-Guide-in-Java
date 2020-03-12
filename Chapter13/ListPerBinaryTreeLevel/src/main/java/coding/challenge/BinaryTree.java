c602c738e6275e0242cbc4475d37690180210d31
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-12 18:01:55 +0200

    List of elements at each depth in a binary tree

---
+++ Chapter13/ListPerBinaryTreeLevel/src/main/java/coding/challenge/BinaryTree.java
@@ -0,0 +1,117 @@
+package coding.challenge;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTree<T> {
+
+    private Node root = null;
+
+    private class Node {
+
+        private Node left;
+        private Node right;
+
+        private final T element;
+
+        public Node(T element) {
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
+
+        public Node(Node left, Node right, T element) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // insert a node into the tree via Breadth-First Search (BFS)
+    public boolean insert(T element) {
+
+        if (element == null) {
+            return false;
+        }
+
+        if (root == null) {
+            root = new Node(element);
+
+            return true;
+        }
+
+        insert(root, element);
+
+        return true;
+    }
+
+    // insert via Breadth-first Search (BFS) algorithm
+    private void insert(Node node, T element) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            node = queue.peek();
+            queue.poll();
+
+            if (node.left == null) {
+                node.left = new Node(element);
+                break;
+            } else {
+                queue.add(node.left);
+            }
+
+            if (node.right == null) {
+                node.right = new Node(element);
+                break;
+            } else {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public List<List<T>> fetchAllLevels() {
+
+        // each queue holds a level
+        List<List<T>> allLevels = new ArrayList<>();
+
+        // first level (containing only the root)
+        Queue<Node> currentLevelOfNodes = new ArrayDeque<>();
+        List<T> currentLevelOfElements = new ArrayList<>();
+
+        currentLevelOfNodes.add(root);
+        currentLevelOfElements.add(root.element);
+
+        while (!currentLevelOfNodes.isEmpty()) {
+
+            // store the current level as the previous level
+            Queue<Node> previousLevelOfNodes = currentLevelOfNodes;
+
+            // add level to the final list
+            allLevels.add(currentLevelOfElements);
+
+            // go to the next level as the current level
+            currentLevelOfNodes = new ArrayDeque<>();
+            currentLevelOfElements = new ArrayList<>();
+
+            // traverse all nodes on current level
+            for (Node parent : previousLevelOfNodes) {
+
+                if (parent.left != null) {
+                    currentLevelOfNodes.add(parent.left);
+                    currentLevelOfElements.add(parent.left.element);
+                }
+                if (parent.right != null) {
+                    currentLevelOfNodes.add(parent.right);
+                    currentLevelOfElements.add(parent.right.element);
+                }
+            }
+        }
+
+        return allLevels;
+    }
+}
---
+++ Chapter13/ListPerBinaryTreeLevel/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,34 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    // [[40], [47, 45], [11, 13, 44, 88], [3, 1, 5, 9]]
+    /*        
+              --40--
+            /        \
+          -47-       -45-
+         /    \     /    \
+        11     13  44    88
+       /  \   /  \
+      3    1 5    9
+    */
+
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        
+        bt.insert(40);
+        bt.insert(47);
+        bt.insert(45);
+        bt.insert(11);
+        bt.insert(13);
+        bt.insert(44);
+        bt.insert(88);
+        bt.insert(3);
+        bt.insert(1);
+        bt.insert(5);
+        bt.insert(9);
+          
+        System.out.println(bt.fetchAllLevels());
+    }
+}
