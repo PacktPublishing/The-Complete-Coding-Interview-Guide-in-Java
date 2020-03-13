4644364ab300362073e6daac110d835a987e226a
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-13 08:40:04 +0200

    Binary tree subtree of another binary tree

---
+++ Chapter13/BinaryTreeSubtree/src/main/java/coding/challenge/BinaryTree.java
@@ -0,0 +1,106 @@
+package coding.challenge;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree<T> {
+
+    private Node root = null;
+
+    private class Node {
+
+        private Node left;
+        private Node right;
+
+        private final T element;
+
+        public Node(T element) {
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
+
+        public Node(Node left, Node right, T element) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // insert a node into the tree via Breadth-First Search (BFS)
+    public boolean insert(T element) {
+
+        if (element == null) {
+            return false;
+        }
+
+        if (root == null) {
+            root = new Node(element);
+
+            return true;
+        }
+
+        insert(root, element);
+
+        return true;
+    }
+
+    // insert via Breadth-first Search (BFS) algorithm
+    private void insert(Node node, T element) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            node = queue.peek();
+            queue.poll();
+
+            if (node.left == null) {
+                node.left = new Node(element);
+                break;
+            } else {
+                queue.add(node.left);
+            }
+
+            if (node.right == null) {
+                node.right = new Node(element);
+                break;
+            } else {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public boolean isSubtree(BinaryTree q) {
+
+        return isSubtree(root, q.root);
+    }
+
+    private boolean isSubtree(Node p, Node q) {
+
+        if (p == null) {
+            return false;
+        }
+
+        if (!match(p, q)) {
+            return (isSubtree(p.left, q) || isSubtree(p.right, q));
+        }
+
+        return true;
+    }
+
+    private boolean match(Node p, Node q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return (p.element == q.element
+                && match(p.left, q.left)
+                && match(p.right, q.right));
+    }
+}
---
+++ Chapter13/BinaryTreeSubtree/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,37 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*    P:                     Q:   
+              40                     30
+            /    \                  /  \ 
+          30      10               20   90 
+         /  \    /  \             /
+        20   90  70 80           6
+       /
+      6  
+        */
+        BinaryTree<Integer> p = new BinaryTree<>();
+
+        p.insert(40);
+        p.insert(30);
+        p.insert(10);
+        p.insert(20);
+        p.insert(90);
+        p.insert(70);
+        p.insert(80);
+        p.insert(6);
+        
+        BinaryTree<Integer> q = new BinaryTree<>();
+
+        q.insert(30);
+        q.insert(20);
+        q.insert(90);
+        q.insert(6);
+        
+        System.out.println("'p' subtree of 'q' ? " + p.isSubtree(q));
+        System.out.println("'q' subtree of 'p' ? " + q.isSubtree(p));
+    }
+}
