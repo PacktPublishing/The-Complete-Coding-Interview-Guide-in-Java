fe8ef568b3a2ee5cf49651175cf995f7e0f36acc
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-13 14:25:30 +0200

    Landing reservation system

--- Chapter13/BinaryTreeLandingReservation/src/main/java/coding/challenge/Main.java
+++ Chapter13/BinaryTreeLandingReservation/src/main/java/coding/challenge/Main.java
@@ -12,13 +12,12 @@ public class Main {
         bst.insert(LocalTime.of(10, 14), 3);
         bst.insert(LocalTime.of(9, 55), 2);
         bst.insert(LocalTime.of(10, 18), 1);
-        bst.insert(LocalTime.of(9, 58), 10);
+        bst.insert(LocalTime.of(9, 58), 5);
         bst.insert(LocalTime.of(9, 50), 6); // cannot do reservation
-        bst.insert(LocalTime.of(9, 47), 2);
-        bst.insert(LocalTime.of(9, 40), 8); // cannot do reservation
-        bst.insert(LocalTime.of(9, 41), 2);
-        bst.insert(LocalTime.of(10, 16), 2); // cannot do reservation
+        bst.insert(LocalTime.of(9, 47), 2);        
+        bst.insert(LocalTime.of(9, 41), 2);        
         bst.insert(LocalTime.of(10, 22), 1);
+        bst.insert(LocalTime.of(10, 4), 4); 
         
         System.out.println("\nThe landing schedule is:");
         bst.printInOrder();
