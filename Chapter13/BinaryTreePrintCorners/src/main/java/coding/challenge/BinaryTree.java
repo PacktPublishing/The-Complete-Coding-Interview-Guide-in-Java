df532e957b40d1ea5cdad7c3f1809f405039d682
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-16 12:07:29 +0200

    Print binary tree corners

---
+++ Chapter13/BinaryTreePrintCorners/src/main/java/coding/challenge/BinaryTree.java
@@ -0,0 +1,119 @@
+package coding.challenge;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree<T> {
+
+    private Node root = null;
+
+    private class Node {
+
+        private Node left;
+        private Node right;
+
+        private final T element;
+
+        private Node(T element) {
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
+
+        private Node(Node left, Node right, T element) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // insert a node into the tree via Breadth-First Search (BFS)
+    public boolean insert(T element) {
+
+        if (element == null) {
+            return false;
+        }
+
+        if (root == null) {
+            root = new Node(element);
+
+            return true;
+        }
+
+        insert(root, element);
+
+        return true;
+    }
+
+    // insert via Breadth-first Search (BFS) algorithm
+    private void insert(Node node, T element) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            node = queue.peek();
+            queue.poll();
+
+            if (node.left == null) {
+                node.left = new Node(element);
+                break;
+            } else {
+                queue.add(node.left);
+            }
+
+            if (node.right == null) {
+                node.right = new Node(element);
+                break;
+            } else {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public void printCorners() {
+
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        int level = 0;
+        while (!queue.isEmpty()) {
+
+            // get the size of the current level
+            int size = queue.size();
+            int position = size;
+
+            System.out.print("Level: " + level + ": ");
+            level++;
+
+            // process all nodes present in current level
+            while (position > 0) {
+
+                Node node = queue.poll();
+                position--;
+
+                // if corner node found, print it
+                if (position == (size - 1) || position == 0) {
+                    System.out.print(node.element + " ");
+                }
+
+                // enqueue left and right child of current node
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            // level done            
+            System.out.println();
+        }
+    }
+}
---
+++ Chapter13/BinaryTreePrintCorners/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,32 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    /*        
+             --40--
+            /      \
+           47       45
+         /    \    /  \
+        11     3  44   23
+       /  \   / 
+      2    4 1   
+    */
+
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        
+        bt.insert(40);
+        bt.insert(47);
+        bt.insert(45);
+        bt.insert(11);
+        bt.insert(3);
+        bt.insert(44);
+        bt.insert(23);
+        bt.insert(2);
+        bt.insert(4);
+        bt.insert(1);        
+                          
+        bt.printCorners();                        
+    }
+}
