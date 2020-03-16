488720a6895f1c8d30b26d6adcf8f1655cac771b
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-16 09:32:01 +0200

    Minimum knight moves

---
+++ Chapter13/ChessKnight/src/main/java/coding/challenge/ChessKnight.java
@@ -0,0 +1,103 @@
+package coding.challenge;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+public class ChessKnight {
+
+    private class Node {
+
+        // (r, c): chess board coordinates, (row, column)
+        // distance: minimum distance from the source (from the starting cell)
+        private final int r;
+        private final int c;
+        private int distance;
+
+        private Node(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        private Node(int r, int c, int distance) {
+            this.r = r;
+            this.c = c;
+            this.distance = distance;
+        }
+    }
+
+    // all 8 possible movements for a knight
+    private static final int[] ROW = {2, 2, -2, -2, 1, 1, -1, -1};
+    private static final int[] COL = {-1, 1, 1, -1, 2, -2, 2, -2};
+
+    public int countknightMoves(int rs, int cs, int rd, int cd, int n) {
+
+        // source (start cell) coordinates
+        Node startCell = new Node(rs, cs);
+
+        // destination (end cell) coordinates
+        Node endCell = new Node(rd, cd);
+
+        return countknightMoves(startCell, endCell, n);
+    }
+
+    // Find minimum number of steps taken by the knight
+    // from the start cell to the end cell using BFS
+    private int countknightMoves(Node startCell, Node endCell, int n) {
+
+        // store the visited cells
+        Set<Node> visited = new HashSet<>();
+
+        // create a queue and enqueue the first cell (the starting cell)
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(startCell);
+
+        while (!queue.isEmpty()) {
+
+            Node cell = queue.poll();
+
+            int r = cell.r;
+            int c = cell.c;
+            int distance = cell.distance;
+
+            // if destination is reached, return the distance
+            if (r == endCell.r && c == endCell.c) {
+                return distance;
+            }
+
+            // the cell was not visited
+            if (!visited.contains(cell)) {
+
+                // mark current cell as visited
+                visited.add(cell);
+
+                // enqueue each valid movement into the queue (there are 8 possible movements)                
+                for (int i = 0; i < 8; ++i) {
+
+                    // get the new valid position of knight from current
+                    // position on chessboard and enqueue it in the
+                    // queue with +1 distance
+                    int rt = r + ROW[i];
+                    int ct = c + COL[i];
+
+                    if (valid(rt, ct, n)) {
+                        queue.add(new Node(rt, ct, distance + 1));
+                    }
+                }
+            }
+        }
+
+        // if path is not possible
+        return Integer.MAX_VALUE;
+    }
+
+    // Check if (r, c) is valid (the knight cannot go out of the chessboard)
+    private static boolean valid(int r, int c, int n) {
+        if (r < 0 || c < 0 || r >= n || c >= n) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Chapter13/ChessKnight/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,13 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ChessKnight ck = new ChessKnight();
+
+        int movesNr = ck.countknightMoves(0, 7, 7, 0, 8);
+
+        System.out.println("Minimum number of required moves: " + movesNr);
+    }
+}
