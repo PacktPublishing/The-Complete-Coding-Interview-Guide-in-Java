9368f2a02dfa667e062e8d35df578588b4d9cc11
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-18 16:40:54 +0200

    Spiral order traversal (Amazon, Microsoft)

---
+++ Chapter13/BinaryTreeSpiralTraversal/src/main/java/coding/challenge/BinaryTree.java
@@ -0,0 +1,262 @@
+package coding.challenge;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BinaryTree<T> {
+
+    private Node root = null;
+
+    private class Node {
+
+        private Node left;
+        private Node right;
+
+        private final T element;
+
+        private Node(T element) {
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
+
+        private Node(Node left, Node right, T element) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // insert a node into the tree via Breadth-First Search (BFS)
+    public boolean insert(T element) {
+
+        if (element == null) {
+            return false;
+        }
+
+        if (root == null) {
+            root = new Node(element);
+
+            return true;
+        }
+
+        insert(root, element);
+
+        return true;
+    }
+
+    // insert via Breadth-first Search (BFS) algorithm
+    private void insert(Node node, T element) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            node = queue.peek();
+            queue.poll();
+
+            if (node.left == null) {
+                node.left = new Node(element);
+                break;
+            } else {
+                queue.add(node.left);
+            }
+
+            if (node.right == null) {
+                node.right = new Node(element);
+                break;
+            } else {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    /* START: Recursive approach in O(n^2) */
+    public void spiralOrderTraversalRecursive() {
+        if (root == null) {
+            return;
+        }
+
+        int level = 1;
+        boolean flip = false;
+
+        // as long as printLevel() returns true there are more levels to print
+        while (printLevel(root, level++, flip = !flip)
+                && printLevel(root, level++, flip = !flip)) {
+            // there is nothing to do
+        };
+    }
+
+    // print all nodes of a given level 
+    private boolean printLevel(Node root, int level, boolean flip) {
+        if (root == null) {
+            return false;
+        }
+
+        if (level == 1) {
+            System.out.print(root.element + " ");
+            return true;
+        }
+
+        if (flip) {
+            // process left child before right child
+            boolean left = printLevel(root.left, level - 1, flip);
+            boolean right = printLevel(root.right, level - 1, flip);
+
+            return left || right;
+        } else {
+            // process right child before left child
+            boolean right = printLevel(root.right, level - 1, flip);
+            boolean left = printLevel(root.left, level - 1, flip);
+
+            return right || left;
+        }
+    }
+
+    /* END: Recursive approach in O(n^2) */
+
+    /* START: Two stacks approach in O(n) and O(n) extra space */
+    public void spiralOrderTraversalTwoStacks() {
+        if (root == null) {
+            return;
+        }
+
+        printSpiral(root);
+    }
+
+    void printSpiral(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        // create two stacks to store alternate levels         
+        Stack<Node> rl = new Stack<>(); // right to left         
+        Stack<Node> lr = new Stack<>(); // left to right 
+
+        // Push first level to first stack 'rl' 
+        rl.push(node);
+
+        // print while any of the stacks has nodes 
+        while (!rl.empty() || !lr.empty()) {
+
+            // print nodes of the current level from 'rl' 
+            // and push nodes of next level to 'lr'
+            while (!rl.empty()) {
+
+                Node temp = rl.peek();
+                rl.pop();
+
+                System.out.print(temp.element + " ");
+
+                if (temp.right != null) {
+                    lr.push(temp.right);
+                }
+
+                if (temp.left != null) {
+                    lr.push(temp.left);
+                }
+            }
+
+            // print nodes of the current level from 'lr' 
+            // and push nodes of next level to 'rl'
+            while (!lr.empty()) {
+                Node temp = lr.peek();
+                lr.pop();
+
+                System.out.print(temp.element + " ");
+
+                if (temp.left != null) {
+                    rl.push(temp.left);
+                }
+                if (temp.right != null) {
+                    rl.push(temp.right);
+                }
+            }
+        }
+    }
+
+    /* END: Two stacks approach in O(n) and O(n) extra space */
+
+    /* START: Deque approach in O(n) and O(n) extra space */
+    public void spiralOrderTraversalDeque() {
+        if (root == null) {
+            return;
+        }
+
+        spiralOrderTraversalDeque(root);
+    }
+
+    private void spiralOrderTraversalDeque(Node root) {
+
+        if (root == null) {
+            return;
+        }
+
+        // create an empty double ended queue 
+        Deque<Node> deque = new ArrayDeque<>();
+
+        // enqueue the root node
+        deque.addFirst(root);
+
+        // odd or even level flag
+        boolean oddEvenFlag = false;
+
+        while (!deque.isEmpty()) {
+
+            // number of nodes in the current level
+            int nodesNo = deque.size();
+
+            // print left to right
+            if (oddEvenFlag) {
+
+                // process each node of the current level 
+                // and enqueue their non-empty left and right child
+                while (nodesNo > 0) {
+
+                    Node currentNode = deque.pollFirst();
+
+                    System.out.print(currentNode.element + " ");
+
+                    // push left child to the end followed by the right child
+                    if (currentNode.left != null) {
+                        deque.addLast(currentNode.left);
+                    }
+
+                    if (currentNode.right != null) {
+                        deque.addLast(currentNode.right);
+                    }
+
+                    nodesNo--;
+                }
+            } else { // print right to left
+
+                // process each node of the current level 
+                // and enqueue their non-empty right and left child
+                while (nodesNo > 0) {
+                    // if 'oddEvenFlag' is false we pop from the back
+                    Node currentNode = deque.pollLast();
+
+                    System.out.print(currentNode.element + " ");
+
+                    // push the right child to front followed by left child
+                    if (currentNode.right != null) {
+                        deque.addFirst(currentNode.right);
+                    }
+
+                    if (currentNode.left != null) {
+                        deque.addFirst(currentNode.left);
+                    }
+
+                    nodesNo--;
+                }
+            }
+
+            // flip the 'oddEvenFlag' for next level
+            oddEvenFlag = !oddEvenFlag;    
+        }
+        /* END: Deque approach in O(n) and O(n) extra space */
+    }
+}
---
+++ Chapter13/BinaryTreeSpiralTraversal/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,51 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    /* Output: 40 45 47 11 3 44 5 87 77 6 2 1 4 23 21 1 5 8 
+               40 47 45 5 44 3 11 21 23 4 1 2 6 77 87 8 5 1 
+              ----40----
+             /          \
+            47          45
+          /    \      /    \  
+         11     3    44     5
+       /   \   / \  /  \   / \
+      21   23 4   1 2  6  77  87
+      /\   /  
+     1  5 8
+    */
+
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        
+        bt.insert(40);
+        
+        bt.insert(47);
+        bt.insert(45);
+        
+        bt.insert(11);
+        bt.insert(3);
+        bt.insert(44);
+        bt.insert(5);
+        
+        bt.insert(21);
+        bt.insert(23);
+        bt.insert(4);
+        bt.insert(1);
+        bt.insert(2);
+        bt.insert(6);
+        bt.insert(77);
+        bt.insert(87);
+        
+        bt.insert(1);
+        bt.insert(5);
+        bt.insert(8);
+          
+        bt.spiralOrderTraversalRecursive();
+        System.out.println();
+        bt.spiralOrderTraversalTwoStacks();
+        System.out.println();
+        bt.spiralOrderTraversalDeque();
+    }
+}
