b08c1f9fe07bc0308701fbc9270dea91fa7ba450
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-18 10:15:22 +0200

    Kth largest element (Amazon, Flipkart, Microsoft)

---
+++ Chapter13/BinarySearchTreeKthLargestElement/src/main/java/coding/challenge/BinarySearchTree.java
@@ -0,0 +1,96 @@
+package coding.challenge;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    private Node root = null;
+
+    private class Node {
+
+        private final T element;
+        private Node left;
+        private Node right;
+
+        private Node(Node left, Node right, T element) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean insert(T element) {
+
+        if (element == null) {
+            return false;
+        }
+
+        if (contains(element)) {
+            return false;
+        }
+
+        root = insert(root, element);
+
+        return true;
+    }
+
+    private Node insert(Node current, T element) {
+
+        if (current == null) {
+            return new Node(null, null, element);
+        }
+
+        if (element.compareTo(current.element) < 0) {
+            current.left = insert(current.left, element);
+        } else {
+            current.right = insert(current.right, element);
+        }
+
+        return current;
+    }
+
+    public boolean contains(T element) {
+
+        return contains(root, element);
+    }
+
+    private boolean contains(Node current, T element) {
+
+        if (current == null || element == null) {
+            return false;
+        }
+
+        if (element.compareTo(current.element) == 0) {
+            return true;
+        }
+
+        return element.compareTo(current.element) < 0
+                ? contains(current.left, element)
+                : contains(current.right, element);
+    }
+
+    private class Counter {
+
+        int c;
+    }
+
+    public void kthLargest(int k) {
+
+        kthLargest(root, k, new Counter());
+    }
+
+    private void kthLargest(Node root, int k, Counter counter) {
+
+        if (root == null || counter.c >= k) {
+            return;
+        }
+
+        kthLargest(root.right, k, counter);
+        counter.c++;
+
+        // we found the kth largest value
+        if (counter.c == k) {
+            System.out.println(root.element);
+        }
+
+        kthLargest(root.left, k, counter);
+    }
+}
---
+++ Chapter13/BinarySearchTreeKthLargestElement/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,36 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    /*        
+              40
+            /    \
+          30      45
+         /  \    /  \
+        15  33  42  56
+       / \      /     \    
+      5  17    41      67
+        
+    */
+
+        BinarySearchTree<Integer> bt = new BinarySearchTree<>();
+        
+        bt.insert(40);
+        bt.insert(30);
+        bt.insert(45);
+        bt.insert(15);
+        bt.insert(33);
+        bt.insert(42);
+        bt.insert(56);
+        bt.insert(5);
+        bt.insert(17);
+        bt.insert(41);
+        bt.insert(67);             
+
+        bt.kthLargest(8);
+    }
+}
+
+
