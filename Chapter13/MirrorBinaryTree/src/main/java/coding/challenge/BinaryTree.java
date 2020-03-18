8f66ba1df6b6f5275512f7207e398b389d3ac9ca
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-18 12:36:14 +0200

    Mirror binary tree

---
+++ Chapter13/MirrorBinaryTree/src/main/java/coding/challenge/BinaryTree.java
@@ -0,0 +1,204 @@
+package coding.challenge;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree<T> {
+
+    private Node root = null;
+
+    private class Node {
+
+        private Node left;
+        private Node right;
+
+        private final T element;
+
+        private Node(T element) {
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
+
+        private Node(Node left, Node right, T element) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public enum TraversalOrder {
+        PRE,
+        IN,
+        POST,
+        LEVEL
+    }
+
+    // insert a node into the tree via Breadth-First Search (BFS)
+    public boolean insert(T element) {
+
+        if (element == null) {
+            return false;
+        }
+
+        if (root == null) {
+            root = new Node(element);
+
+            return true;
+        }
+
+        insert(root, element);
+
+        return true;
+    }
+
+    // insert via Breadth-first Search (BFS) algorithm
+    private void insert(Node node, T element) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            node = queue.peek();
+            queue.poll();
+
+            if (node.left == null) {
+                node.left = new Node(element);
+                break;
+            } else {
+                queue.add(node.left);
+            }
+
+            if (node.right == null) {
+                node.right = new Node(element);
+                break;
+            } else {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    // mirror tree in another tree
+    public BinaryTree mirrorTreeInTree() {
+
+        Node mirroredRoot = mirrorTreeInTree(root);
+        BinaryTree<T> mirroredBt = new BinaryTree();
+
+        mirroredBt.root = mirroredRoot;
+
+        return mirroredBt;
+    }
+
+    // mirror tree in another tree
+    private Node mirrorTreeInTree(Node root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        Node node = new Node(root.element);
+        node.left = mirrorTreeInTree(root.right);
+        node.right = mirrorTreeInTree(root.left);
+
+        return node;
+    }
+
+    // mirror tree in place
+    public void mirrorTreeInPlace() {
+
+        mirrorTreeInPlace(root);
+    }
+
+    // mirror tree in place
+    private void mirrorTreeInPlace(Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        Node auxNode;
+
+        mirrorTreeInPlace(node.left);
+        mirrorTreeInPlace(node.right);
+
+        auxNode = node.left;
+        node.left = node.right;
+        node.right = auxNode;
+    }
+
+// print tree via Depth-First Search (DFS) and Breadth-First Search (BFS) 
+    public void print(TraversalOrder to) {
+
+        if (root == null) {
+            System.out.println("empty");
+            return;
+        }
+
+        switch (to) {
+            // DFS
+            case IN:
+                printInOrder(root);
+                break;
+            case PRE:
+                printPreOrder(root);
+                break;
+            case POST:
+                printPostOrder(root);
+                break;
+            // BFS
+            case LEVEL:
+                printLevelOrder(root);
+                break;
+            default:
+                throw new IllegalArgumentException("Unrecognized traversal order");
+        }
+    }
+
+    private void printInOrder(Node node) {
+        if (node != null) {
+            printInOrder(node.left);
+            System.out.print(" " + node.element);
+            printInOrder(node.right);
+        }
+    }
+
+    private void printPreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.element);
+            printPreOrder(node.left);
+            printPreOrder(node.right);
+        }
+    }
+
+    private void printPostOrder(Node node) {
+        if (node != null) {
+            printPostOrder(node.left);
+            printPostOrder(node.right);
+            System.out.print(" " + node.element);
+        }
+    }
+
+    private void printLevelOrder(Node node) {
+
+        Queue<Node> queue = new ArrayDeque<>();
+
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+
+            Node current = queue.poll();
+
+            System.out.print(" " + current.element);
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+
+}
---
+++ Chapter13/MirrorBinaryTree/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,64 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    /*                          
+              40                40
+            /    \            /    \
+          47      45         45    47
+         /  \    /            \   /  \
+        11   3  44            44 3    11
+    */
+
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        
+        bt.insert(40);
+        bt.insert(47);
+        bt.insert(45);
+        bt.insert(11);
+        bt.insert(3);
+        bt.insert(44);
+                      
+        System.out.println("Given tree");
+        System.out.println("----------");
+        
+        System.out.println("\nTraversal LEVEL: ");
+        bt.print(BinaryTree.TraversalOrder.LEVEL);
+        
+        System.out.println("\n\nTraversal IN-ORDER: ");
+        bt.print(BinaryTree.TraversalOrder.IN);        
+        
+        System.out.println("\n\nTraversal PRE-ORDER: ");
+        bt.print(BinaryTree.TraversalOrder.PRE);        
+        
+        System.out.println("\n\nMirorred tree in a new tree");
+        System.out.println("---------------------------");
+        
+        BinaryTree<Integer> mirroredBt = bt.mirrorTreeInTree();
+        
+        System.out.println("\nTraversal LEVEL (mirror in new tree): ");
+        mirroredBt.print(BinaryTree.TraversalOrder.LEVEL);
+        
+        System.out.println("\n\nTraversal IN-ORDER (mirror in new tree): ");
+        mirroredBt.print(BinaryTree.TraversalOrder.IN);                        
+        
+        System.out.println("\n\nTraversal PRE-ORDER (mirror in new tree): ");
+        mirroredBt.print(BinaryTree.TraversalOrder.PRE);        
+        
+        System.out.println("\n\nMirorred tree in place");
+        System.out.println("----------------------");
+        
+        bt.mirrorTreeInPlace();
+        
+        System.out.println("\nTraversal LEVEL (mirror in place): ");
+        bt.print(BinaryTree.TraversalOrder.LEVEL);
+        
+        System.out.println("\n\nTraversal IN-ORDER (mirror in place): ");
+        bt.print(BinaryTree.TraversalOrder.IN);        
+        
+        System.out.println("\n\nTraversal PRE-ORDER (mirror in place): ");
+        bt.print(BinaryTree.TraversalOrder.PRE);        
+    }
+}
