19cb46c18345d50398dd5fd308b6cd4b06272d2b
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-19 16:32:25 +0200

    Connect n ropes with minimum cost

--- Chapter13/HeapConnectRopes/src/main/java/coding/challenge/Main.java
+++ Chapter13/HeapConnectRopes/src/main/java/coding/challenge/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] ropesLength = {2, 5, 4, 1, 5, 7, 4};
+        int[] ropesLength = {1, 4, 6, 3};
         MinHeap heap = new MinHeap();
         
         int result = heap.minimumCost(ropesLength);
