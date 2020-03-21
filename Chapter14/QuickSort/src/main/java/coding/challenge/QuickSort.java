a47ed558680f8d37001a80eb9b8f1a7a20b26978
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-21 14:56:41 +0200

    Quick sort

---
+++ Chapter14/QuickSort/src/main/java/coding/challenge/QuickSort.java
@@ -0,0 +1,78 @@
+package coding.challenge;
+
+import java.util.Comparator;
+
+public final class QuickSort {
+
+    private QuickSort() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    /* Quick sort for primitives (e.g., integers) */
+    public static void sort(int[] arr, int left, int right) {
+
+        if (left < right) {
+            int pivot = partition(arr, left, right);
+
+            sort(arr, left, pivot - 1);
+            sort(arr, pivot + 1, right);
+        }
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+
+        int pivot = arr[right];
+        int m = left;
+        for (int i = m; i < right; i++) {
+            if (arr[i] <= pivot) {
+                swap(arr, i, m++);
+            }
+        }
+
+        swap(arr, right, m);
+
+        return m;
+    }
+
+    /* Quick sort with Comparator */
+    public static <T> void sortWithComparator(
+            T[] arr, int left, int right, Comparator<? super T> c) {
+
+        if (left < right) {
+            int pivot = partitionWithComparator(arr, left, right, c);
+
+            sortWithComparator(arr, left, pivot - 1, c);
+            sortWithComparator(arr, pivot + 1, right, c);
+        }
+    }
+
+    private static <T> int partitionWithComparator(
+            T[] arr, int left, int right, Comparator<? super T> c) {
+
+        T pivot = arr[right];
+        int m = left;
+        for (int i = m; i < right; i++) {
+            if (c.compare(arr[i], pivot) <= 0) {
+                swap(arr, i, m++);
+            }
+        }
+
+        swap(arr, right, m);
+
+        return m;
+    }
+
+    /* Helper methods */
+    private static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    private static <T> void swap(T[] arr, int x, int y) {
+        T temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+}
