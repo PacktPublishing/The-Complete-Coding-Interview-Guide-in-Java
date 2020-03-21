774abb00f18974ae6c3b52e7f52c32794276e58d
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-21 09:07:27 +0200

    14 sorting algorithms implementations

---
+++ Chapter14/SortArraysIn14Ways/src/main/java/coding/challenge/Melon.java
@@ -0,0 +1,25 @@
+package coding.challenge;
+
+public class Melon {
+
+    private final String type;
+    private final int weight;
+
+    public Melon(String type, int weight) {
+        this.type = type;
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return type + "(" + weight + "g)";
+    }
+}
