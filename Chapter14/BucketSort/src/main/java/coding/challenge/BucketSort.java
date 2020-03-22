2e850bcfe7ad5d5b114492fa11b91538932b6204
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-22 14:06:13 +0200

    Bucket sort

--- Chapter14/BucketSort/src/main/java/coding/challenge/BucketSort.java
+++ Chapter14/BucketSort/src/main/java/coding/challenge/BucketSort.java
@@ -1,17 +1,70 @@
 package coding.challenge;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public final class BucketSort {
 
     private BucketSort() {
         throw new AssertionError("Cannot be instantiated");
     }
 
-    public static void sort(int[] arr) {
+    /* Scatter-Sort-Gather approach */
+    public static void sort1(int[] arr) {
+
+        // get the hash codes 
+        int[] hashes = hash(arr);
+
+        // create and initialize buckets
+        List<Integer>[] buckets = new List[hashes[1]];
+        for (int i = 0; i < hashes[1]; i++) {
+            buckets[i] = new ArrayList();
+        }
+
+        // scatter elements into buckets
+        for (int e : arr) {
+            buckets[hash(e, hashes)].add(e);
+        }
+
+        // sort each bucket, O(n) 
+        for (List bucket : buckets) {
+            Collections.sort(bucket);
+        }
+
+        // gather elements from the buckets
+        int p = 0;
+        for (List<Integer> bucket : buckets) {
+            for (int j : bucket) {
+                arr[p++] = j;
+            }
+        }
+    }
+
+    private static int[] hash(int[] arr) {
+
+        int m = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (m < arr[i]) {
+                m = arr[i];
+            }
+        }
+
+        return new int[]{m, (int) Math.sqrt(arr.length)};
+    }
+
+    private static int hash(int num, int[] hashes) {
+        return (int) ((double) num / hashes[0] * (hashes[1] - 1));
+    }
+
+    /* Scatter-Gather approach */
+    public static void sort2(int[] arr) {
 
         if (arr == null) {
             throw new IllegalArgumentException("Array cannot be null");
         }
 
+        // get the maximum value of the given array
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
@@ -19,16 +72,21 @@ public final class BucketSort {
             }
         }
 
+        // create max buckets
         int[] bucket = new int[max + 1];
 
+        // in Java, the bucket[] is automatically initialized with 0s, 
+        // therefore this step is redundant
         for (int i = 0; i < bucket.length; i++) {
             bucket[i] = 0;
         }
 
+        // scatter elements in buckets
         for (int i = 0; i < arr.length; i++) {
             bucket[arr[i]]++;
         }
 
+        // gather elements from the buckets
         int p = 0;
         for (int i = 0; i < bucket.length; i++) {
             for (int j = 0; j < bucket[i]; j++) {
--- Chapter14/BucketSort/src/main/java/coding/challenge/Main.java
+++ Chapter14/BucketSort/src/main/java/coding/challenge/Main.java
@@ -7,12 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         // initialize an array integers        
-        int[] integers = {4, 5, 2, 7, 1};
+        int[] integers = {4, 1, 8, 3, 8, 2, 6, 7, 4, 9};
 
         System.out.println("\n\nSort via Bucket sort:");
         System.out.println("-------------------------");
 
-        BucketSort.sort(integers);
-        System.out.println("Sorted: " + Arrays.toString(integers));
+        int[] integersC1 = integers.clone();
+        int[] integersC2 = integers.clone();
+        
+        BucketSort.sort1(integersC1);
+        BucketSort.sort1(integersC2);
+        System.out.println("Sorted C1: " + Arrays.toString(integersC1));
+        System.out.println("Sorted C2: " + Arrays.toString(integersC2));
     }
 }
