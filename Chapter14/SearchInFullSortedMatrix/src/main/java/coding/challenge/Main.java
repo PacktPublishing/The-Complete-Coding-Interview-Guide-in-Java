92f7f25e46598f2adb614fafba9655ea8f587d7e
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-24 11:37:36 +0200

    Search in full sorted matrix

--- Chapter14/SearchInFullSortedMatrix/src/main/java/coding/challenge/Main.java
+++ Chapter14/SearchInFullSortedMatrix/src/main/java/coding/challenge/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[][] m = {
-            {1, 3, 7, 8, 9},
+            {1, 3, 7, 8},
             {10, 16, 19, 22},
             {28, 36, 39, 53}
         };
