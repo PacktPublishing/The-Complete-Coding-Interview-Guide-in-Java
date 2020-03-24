b063a598b66b91cced568f15f03230dd4882c71f
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-24 17:26:18 +0200

    Search in sorted matrix

---
+++ Chapter14/SearchInSortedMatrix/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,35 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {11, 22, 48, 77, 78, 84},
+            {12, 24, 55, 78, 83, 90},
+            {25, 56, 58, 80, 85, 95},
+            {33, 57, 60, 85, 86, 99}
+        };
+
+        int elementToFind1 = 80;
+        int elementToFind2 = 77;
+        int elementToFind3 = 92;
+
+        System.out.println("Iterative approach (" + elementToFind1 + "): "
+                + Matrices.search(matrix, elementToFind1));
+        System.out.println("Iterative approach (" + elementToFind2 + "): "
+                + Matrices.search(matrix, elementToFind2));
+        System.out.println("Iterative approach (" + elementToFind3 + "): "
+                + Matrices.search(matrix, elementToFind3));
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Recursive approach (" + elementToFind1 + "): "
+                + Matrices.searchRecursive(matrix, elementToFind1));
+        System.out.println("Recursive approach (" + elementToFind2 + "): "
+                + Matrices.searchRecursive(matrix, elementToFind2));
+        System.out.println("Recursive approach (" + elementToFind3 + "): "
+                + Matrices.searchRecursive(matrix, elementToFind3));
+    }
+}
---
+++ Chapter14/SearchInSortedMatrix/src/main/java/coding/challenge/Matrices.java
@@ -0,0 +1,60 @@
+package coding.challenge;
+
+public final class Matrices {
+
+    private Matrices() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    /* Iterative approach (searchRecursive() listed below is the recursive code) */
+    public static boolean search(int[][] matrix, int element) {
+
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            
+            if (matrix[row][col] == element) {
+                return true;
+            } else if (matrix[row][col] > element) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+
+    /* Search via recursion - this is the recursive approach of the above search() method */
+    public static boolean searchRecursive(int[][] matrix, int element) {
+
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        return searchRecursive(matrix, matrix.length - 1, 0, matrix[0].length - 1, element);
+    }
+
+    private static boolean searchRecursive(int[][] matrix, int row, int col,
+            int cols, int element) {
+
+        if (row < 0 || col > cols) {
+            return false;
+        }
+
+        if (matrix[row][col] == element) {
+            return true;
+        } else {
+            if (element > matrix[row][col]) {
+                return searchRecursive(matrix, row, col + 1, cols, element);
+            } else {
+                return searchRecursive(matrix, row - 1, col, cols, element);
+            }
+        }
+    }
+}
