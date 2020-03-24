1f63948dffdefb0fde30cc5c86d555b48c71588c
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-24 09:30:05 +0200

    Strings interspersed with empty strings

---
+++ Chapter14/InterspersedEmptyStrings/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,17 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] stringsArr = {"cat", "", "", "house", "", "", "", "", "laptop",
+            "pen", "", "", "", "", "rear", "", "tower", "", ""};
+
+        System.out.println("Search 'join':" + Strings.search(stringsArr, "join"));
+        System.out.println("Search 'pen':" + Strings.search(stringsArr, "pen"));
+        System.out.println("Search 'tower':" + Strings.search(stringsArr, "tower"));
+        System.out.println("Search 'cat':" + Strings.search(stringsArr, "cat"));       
+        System.out.println("Search 'laptop':" + Strings.search(stringsArr, "laptop"));
+        System.out.println("Search '':" + Strings.search(stringsArr, ""));
+    }
+}
---
+++ Chapter14/InterspersedEmptyStrings/src/main/java/coding/challenge/Strings.java
@@ -0,0 +1,62 @@
+package coding.challenge;
+
+public final class Strings {
+
+    private Strings() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int search(String[] stringsArr, String str) {
+
+        if (stringsArr == null || str == null || str.isBlank()) {
+            return -1;
+        }
+
+        return search(stringsArr, str, 0, stringsArr.length - 1);
+    }
+
+    private static int search(String[] stringsArr, String str, int left, int right) {
+
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+
+        // since mid is empty we try to find the closest non-empty string to mid
+        if (stringsArr[mid].isEmpty()) {
+
+            int leftMid = mid - 1;
+            int rightMid = mid + 1;
+
+            while (true) {
+
+                if (leftMid < left && rightMid > right) {
+                    return -1;
+
+                } else if (rightMid <= right && !stringsArr[rightMid].isEmpty()) {
+                    mid = rightMid;
+                    break;
+
+                } else if (leftMid >= left && !stringsArr[leftMid].isEmpty()) {
+                    mid = leftMid;
+                    break;
+                }
+
+                rightMid++;
+                leftMid--;
+            }
+        }
+
+        if (str.equals(stringsArr[mid])) {
+            // the searched string was found
+            return mid;
+        } else if (stringsArr[mid].compareTo(str) < 0) {
+            // search to the right
+            return search(stringsArr, str, mid + 1, right);
+        } else {
+            // search to the left
+            return search(stringsArr, str, left, mid - 1);
+        }
+    }
+}
