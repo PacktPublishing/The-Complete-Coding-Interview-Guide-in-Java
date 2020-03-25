3b757a4cca88ea74cab037afb3881ef0bb95d339
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-25 09:47:27 +0200

    Stream rank

--- Chapter14/RankInStream/src/main/java/coding/challenge/Main.java
+++ Chapter14/RankInStream/src/main/java/coding/challenge/Main.java
@@ -10,8 +10,8 @@ public class Main {
           30      45
          /  \    /  \
         15  33  42  56
-       / \      /     \    
-      5  17    41      67        
+       / \       \    \    
+      5  17       43  67        
       */
         Stream stream = new Stream();
 
@@ -24,7 +24,7 @@ public class Main {
         stream.generate(56);
         stream.generate(5);
         stream.generate(17);
-        stream.generate(41);
+        stream.generate(43);
         stream.generate(67);
 
         System.out.println("Rank of 40: " + stream.getRank(40));
@@ -36,7 +36,7 @@ public class Main {
         System.out.println("Rank of 56: " + stream.getRank(56));
         System.out.println("Rank of 5: " + stream.getRank(5));
         System.out.println("Rank of 17: " + stream.getRank(17));
-        System.out.println("Rank of 41: " + stream.getRank(41));
+        System.out.println("Rank of 43: " + stream.getRank(43));
         System.out.println("Rank of 67: " + stream.getRank(67));
         System.out.println("Rank of 6: " + stream.getRank(6));
     }
