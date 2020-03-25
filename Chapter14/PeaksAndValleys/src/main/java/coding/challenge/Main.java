d40ac6394975f70a8e826118f7e40ef852eec0df
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-25 14:59:00 +0200

    Peaks and valleys

---
+++ Chapter14/PeaksAndValleys/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,16 @@
+package coding.challenge;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] integers = {4, 5, 8, 3, 2, 1, 7, 8, 5, 9};
+
+        System.out.println("Before sorting: " + Arrays.toString(integers));
+        PeaksValleys.sort(integers);
+        System.out.println("After sorting: " + Arrays.toString(integers));
+
+    }
+}
---
+++ Chapter14/PeaksAndValleys/src/main/java/coding/challenge/PeaksValleys.java
@@ -0,0 +1,46 @@
+package coding.challenge;
+
+import java.util.Arrays;
+
+public final class PeaksValleys {
+
+    private PeaksValleys() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static void sort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i += 2) {
+            
+            int maxFoundIndex = maxElementIndex(arr, i - 1, i, i + 1);
+            if (i != maxFoundIndex) {
+                swap(arr, i, maxFoundIndex);
+            }            
+        }
+    }
+
+    private static int maxElementIndex(int[] arr, int left, int middle, int right) {
+
+        int arrLength = arr.length;
+        int leftElement = left >= 0 && left < arrLength ? arr[left] : Integer.MIN_VALUE;
+        int middleElement = middle >= 0 && middle < arrLength ? arr[middle] : Integer.MIN_VALUE;
+        int rightElement = right >= 0 && right < arrLength ? arr[right] : Integer.MIN_VALUE;
+
+        int maxElement = Math.max(leftElement, Math.max(middleElement, rightElement));
+
+        if (leftElement == maxElement) {
+            return left;
+        } else if (middleElement == maxElement) {
+            return middle;
+        } else {
+            return right;
+        }
+    }
+
+    /* helper method to swap two elements of the array */
+    private static void swap(int[] arr, int x, int y) {       
+        int z = arr[x];
+        arr[x] = arr[y];
+        arr[y] = z;
+    }
+}
