e4a233783edeeb550ecf4be2d3ce27183e64527c
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-25 08:31:16 +0200

    Stream rank

---
+++ Chapter14/RankInStream/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,43 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      /*        
+              40
+            /    \
+          30      45
+         /  \    /  \
+        15  33  42  56
+       / \      /     \    
+      5  17    41      67        
+      */
+        Stream stream = new Stream();
+
+        stream.generate(40);
+        stream.generate(30);
+        stream.generate(45);
+        stream.generate(15);
+        stream.generate(33);
+        stream.generate(42);
+        stream.generate(56);
+        stream.generate(5);
+        stream.generate(17);
+        stream.generate(41);
+        stream.generate(67);
+
+        System.out.println("Rank of 40: " + stream.getRank(40));
+        System.out.println("Rank of 30: " + stream.getRank(30));
+        System.out.println("Rank of 45: " + stream.getRank(45));
+        System.out.println("Rank of 15: " + stream.getRank(15));
+        System.out.println("Rank of 33: " + stream.getRank(33));
+        System.out.println("Rank of 42: " + stream.getRank(42));
+        System.out.println("Rank of 56: " + stream.getRank(56));
+        System.out.println("Rank of 5: " + stream.getRank(5));
+        System.out.println("Rank of 17: " + stream.getRank(17));
+        System.out.println("Rank of 41: " + stream.getRank(41));
+        System.out.println("Rank of 67: " + stream.getRank(67));
+        System.out.println("Rank of 6: " + stream.getRank(6));
+    }
+}
---
+++ Chapter14/RankInStream/src/main/java/coding/challenge/Stream.java
@@ -0,0 +1,81 @@
+package coding.challenge;
+
+public class Stream {
+
+    private Node root = null;
+
+    private class Node {
+
+        private final int element;
+        public int leftTreeSize;
+        private Node left;
+        private Node right;        
+
+        private Node(int element) {
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
+
+        private Node(Node left, Node right, int element) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /* add a new node into the tree */
+    public void generate(int element) {
+
+        if (root == null) {
+            root = new Node(element);
+        } else {
+            insert(root, element);
+        }
+    }
+
+    private void insert(Node node, int element) {
+
+        if (element <= node.element) {
+            if (node.left != null) {
+                insert(node.left, element);
+            } else {
+                node.left = new Node(element);
+            }
+            node.leftTreeSize++;
+        } else {
+            if (node.right != null) {
+                insert(node.right, element);
+            } else {
+                node.right = new Node(element);
+            }
+        }
+    }
+
+    /* return rank of 'element' */
+    public int getRank(int element) {
+        return getRank(root, element);
+    }
+
+    private int getRank(Node node, int element) {
+
+        if (element == node.element) {
+            return node.leftTreeSize;
+        } else if (element < node.element) {
+            if (node.left == null) {
+                return -1;
+            } else {
+                return getRank(node.left, element);
+            }
+        } else {
+            int rightTreeRank = node.right == null 
+                    ? -1 : getRank(node.right, element);
+            
+            if (rightTreeRank == -1) {
+                return -1;
+            } else {
+                return node.leftTreeSize + 1 + rightTreeRank;
+            }
+        }
+    }
+}
