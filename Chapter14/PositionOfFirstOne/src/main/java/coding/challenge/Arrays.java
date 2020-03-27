3dfe8ead49c6f43dfcd278712a6c64b91c2a1ac1
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-27 15:30:56 +0200

    Position of first one (Adobe, Amazon, Microsoft)

---
+++ Chapter14/PositionOfFirstOne/src/main/java/coding/challenge/Arrays.java
@@ -0,0 +1,35 @@
+package coding.challenge;
+
+public final class Arrays {
+
+    private Arrays() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int firstOneIndex(int[] arr) {
+
+        if (arr == null) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            
+            int middle = 1 + (right - left) / 2;
+            
+            if (arr[middle] == 0) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+            
+            if (arr[left] == 1) {
+                return left;
+            }
+        }
+        
+        return -1;
+    }
+}
---
+++ Chapter14/PositionOfFirstOne/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,12 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] zerosAndOnes = {0, 0, 1, 1, 1, 1, 1};
+
+        int index = Arrays.firstOneIndex(zerosAndOnes);
+        System.out.println("Index: " + index);
+    }
+}
