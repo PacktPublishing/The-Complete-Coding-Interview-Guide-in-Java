c7859c24530a91ff8020c0bb23eb3a172e3a4929
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-27 15:14:58 +0200

    Word Search (Amazon, Google)

--- Chapter14/WordSearch/src/main/java/coding/challenge/Main.java
+++ Chapter14/WordSearch/src/main/java/coding/challenge/Main.java
@@ -14,7 +14,7 @@ public class Main {
                 };               
         
         boolean resultTable = Words.exist(board, "TABLE");
-        boolean resultTaxi = Words.exist(board, "TAXE");
+        boolean resultTaxi = Words.exist(board, "TAXI");
 
         System.out.println("Found 'TABLE'? " + resultTable);
         System.out.println("Found 'TAXI'? " + resultTaxi);
