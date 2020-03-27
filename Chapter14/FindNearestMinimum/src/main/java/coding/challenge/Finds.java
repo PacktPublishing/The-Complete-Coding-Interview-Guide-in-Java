46d1a742a46dfea4811047706a467ce3f6b91978
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-27 08:21:52 +0200

    Find nearest smaller left (Adobe, Amazon, Flipkart, Microsoft)

--- Chapter14/FindNearestMinimum/src/main/java/coding/challenge/Finds.java
+++ Chapter14/FindNearestMinimum/src/main/java/coding/challenge/Finds.java
@@ -12,16 +12,22 @@ public final class Finds {
 
         Stack<Integer> stack = new Stack<>();
 
+        // While the top element of the stack is greater than 
+        // equal to arr[i] remove it from the stack        
         for (int i = 0; i < arr.length; i++) {
             while (!stack.empty() && stack.peek() >= arr[i]) {
                 stack.pop();
             }
 
+            // if stack is empty there is no left smaller element
             if (stack.empty()) {
                 System.out.print("_, ");
             } else {
+                // the top of the stack is the left smaller element
                 System.out.print(stack.peek() + ", ");
             }
+            
+            // push arr[i] into the stack
             stack.push(arr[i]);
         }
     }
