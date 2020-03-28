177a26057da76ed44b32973a00496f249b6cf913
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-28 09:55:29 +0200

    FizzBuzz (Adobe, Microsoft)

---
+++ Chapter15/FizzBuzz/src/main/java/coding/challenge/FizzBuzz.java
@@ -0,0 +1,26 @@
+package coding.challenge;
+
+public final class FizzBuzz {
+
+    private FizzBuzz() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static void print(int n) {
+        
+        for (int i = 1; i <= n; i++) {
+            if (((i % 5) == 0) && ((i % 7) == 0)) // multiple of 5 & 7
+            {
+                System.out.println("fizzbuzz");
+            } else if ((i % 5) == 0) // multiple of 5
+            {
+                System.out.println("fizz");
+            } else if ((i % 7) == 0) // multiple of 7
+            {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i); // not a multiple of 5 or 7
+            }
+        }
+    }
+}
---
+++ Chapter15/FizzBuzz/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,9 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FizzBuzz.print(100);
+    }
+}
