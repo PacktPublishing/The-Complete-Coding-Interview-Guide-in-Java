3e85ae5c64b5cba535d2bba0297efddd9be98925
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-28 10:02:42 +0200

    FizzBuzz (Adobe, Microsoft)

--- Chapter15/FizzBuzz/src/main/java/coding/challenge/FizzBuzz.java
+++ Chapter15/FizzBuzz/src/main/java/coding/challenge/FizzBuzz.java
@@ -7,16 +7,13 @@ public final class FizzBuzz {
     }
 
     public static void print(int n) {
-        
+
         for (int i = 1; i <= n; i++) {
-            if (((i % 5) == 0) && ((i % 7) == 0)) // multiple of 5 & 7
-            {
+            if (((i % 5) == 0) && ((i % 7) == 0)) { // multiple of 5 & 7            
                 System.out.println("fizzbuzz");
-            } else if ((i % 5) == 0) // multiple of 5
-            {
+            } else if ((i % 5) == 0) { // multiple of 5            
                 System.out.println("fizz");
-            } else if ((i % 7) == 0) // multiple of 7
-            {
+            } else if ((i % 7) == 0) { // multiple of 7            
                 System.out.println("buzz");
             } else {
                 System.out.println(i); // not a multiple of 5 or 7
