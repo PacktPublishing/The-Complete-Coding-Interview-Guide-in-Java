e2b42382cfed0c9342fe068e00ef5fb5f55c740b
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-30 09:22:19 +0300

    ABCD to CDBA (Flipkart, Microsoft)

---
+++ Chapter15/Abcd/src/main/java/coding/challenge/Abcd.java
@@ -0,0 +1,27 @@
+package coding.challenge;
+
+public final class Abcd {
+
+    private Abcd() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static void find() {
+        for (int i = 1000; i < 2499; i++) {
+
+            int p = i;
+            int q = i * 4;
+
+            String m = String.valueOf(p);
+            String n = new StringBuilder(String.valueOf(q)).reverse().toString();
+
+            p = Integer.parseInt(m);
+            q = Integer.parseInt(n);
+
+            if (p == q) {
+                System.out.println("\n\nFound: " + p + " : " + (q * 4));
+                break;
+            }
+        }
+    }
+}
