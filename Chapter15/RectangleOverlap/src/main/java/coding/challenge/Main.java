53d2904e51150252378d6b09325549918ee1c18c
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-30 15:14:00 +0300

    Rectangle overlapping (Amazon, Microsoft)

---
+++ Chapter15/RectangleOverlap/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,19 @@
+package coding.challenge;
+
+import java.awt.Point;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Point r1lt = new Point(1, 8);
+        Point r1rb = new Point(5, 6);
+
+        Point r2lt = new Point(3, 7);
+        Point r2rb = new Point(6, 5);
+
+        boolean result1 = Rectangles.overlap1(r1lt, r1rb, r2lt, r2rb);
+        boolean result2 = Rectangles.overlap2(r1lt, r1rb, r2lt, r2rb);
+        System.out.println("Result: " + result1 + "  |  " + result2);
+    }
+}
---
+++ Chapter15/RectangleOverlap/src/main/java/coding/challenge/Rectangles.java
@@ -0,0 +1,31 @@
+package coding.challenge;
+
+import java.awt.Point;
+
+public final class Rectangles {
+
+    private Rectangles() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static boolean overlap1(Point r1lt, Point r1rb, Point r2lt, Point r2rb) {
+
+        return (r1lt.x <= r2rb.x && r1rb.x >= r2lt.x
+                && r1lt.y >= r2rb.y && r1rb.y <= r2lt.y);
+    }
+    
+    public static boolean overlap2(Point r1lt, Point r1rb, Point r2lt, Point r2rb) {
+
+        // r1 is totally to the right of r2 or vice versa
+        if (r1lt.x > r2rb.x || r2lt.x > r1rb.x) {
+            return false;
+        }
+
+        // r1 is totally above r2 or vice versa
+        if (r1rb.y > r2lt.y || r2rb.y > r1lt.y) {
+            return false;
+        }
+
+        return true;
+    }
+}
