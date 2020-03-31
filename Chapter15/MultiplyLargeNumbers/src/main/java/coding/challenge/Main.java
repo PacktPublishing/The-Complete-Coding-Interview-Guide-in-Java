bfe83136c9f4dc83cd764168575b1a1cfe6523ae
AnghelLeonard <leoprivacy@yahoo.com>
2020-03-31 09:20:36 +0300

    Multiply large numbers (Amazon, Microsoft)

--- Chapter15/MultiplyLargeNumbers/src/main/java/coding/challenge/Main.java
+++ Chapter15/MultiplyLargeNumbers/src/main/java/coding/challenge/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // result 18023800768597293570
-        String a = "34342343244234223";
-        String b = "32212789565756554354435432";
+        // result 3198328601925
+        String a = "4145775";
+        String b = "771467";
                
         String result = Numbers.multiply(a, b);
         System.out.print(result);
