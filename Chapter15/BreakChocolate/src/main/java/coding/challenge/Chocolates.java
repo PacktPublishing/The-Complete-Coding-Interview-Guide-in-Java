31c6499f7c743b152ce9946e3dede270080f63a4
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-06 08:09:14 +0300

    Flipkart

---
+++ Chapter15/BreakChocolate/src/main/java/coding/challenge/Chocolates.java
@@ -0,0 +1,41 @@
+package coding.challenge;
+
+public final class Chocolates {
+
+    private Chocolates() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int breakit(int width, int height, int nTiles) {
+
+        if (width <= 0 || height <= 0 || nTiles <= 0) {
+            return -1;
+        }
+
+        if (width * height < nTiles) {
+            return -1;
+        }
+
+        if (width * height == nTiles) {
+            return 0;
+        }
+
+        if ((nTiles % width == 0 && (nTiles / width) < height)
+                || (nTiles % height == 0 && (nTiles / height) < width)) {
+            return 1;
+        }
+
+        for (int i = 1; i <= Math.sqrt(nTiles); i++) {
+            if (nTiles % i == 0) {
+                int a = i;
+                int b = nTiles / i;
+                if ((a <= width && b <= height)
+                        || (a <= height && b <= width)) {
+                    return 2;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
