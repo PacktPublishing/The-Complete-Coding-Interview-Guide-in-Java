74b937caa6cb4d6547ccf2f0dfec4755a165efe0
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-07 12:57:58 +0300

    Find position of the only set bit (Adobe, Microsoft)

---
+++ Chapter09/PositionOfFirstBitOfOne/src/main/java/coding/challenge/Bits.java
@@ -0,0 +1,29 @@
+package coding.challenge;
+
+public final class Bits {
+
+    private Bits() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int findPosition(int n) {
+                  
+        int count = 0;
+        
+        if(!isPowerOfTwo(n)) {
+            return -1;
+        }
+                
+        while (n != 0) {
+            n = n >> 1;          
+            ++count;
+        }
+
+        return count;
+    }
+
+    private static boolean isPowerOfTwo(int n) {
+
+        return (n > 0) && ((n & (n - 1)) == 0);
+    }
+}
---
+++ Chapter09/PositionOfFirstBitOfOne/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,12 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 4;
+        
+        System.out.println("Result: " + Bits.findPosition(number));
+    }
+
+}
