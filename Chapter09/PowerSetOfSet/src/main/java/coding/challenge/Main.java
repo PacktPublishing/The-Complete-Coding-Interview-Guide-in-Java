be244aca6c7a5f0a85fb1e725f5907b02143def9
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-07 16:57:58 +0300

    Power set of a set (Adobe)

---
+++ Chapter09/PowerSetOfSet/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,16 @@
+package coding.challenge;
+
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char[] set = {'a', 'b', 'c'};
+        
+        Set<Set<Character>> subsets = Sets.powerSet(set);
+        
+        System.out.println("Result: " + subsets);
+    }
+
+}
---
+++ Chapter09/PowerSetOfSet/src/main/java/coding/challenge/Sets.java
@@ -0,0 +1,41 @@
+package coding.challenge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Sets {
+
+    private Sets() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static Set<Set<Character>> powerSet(char[] set) {
+
+        if (set == null) {
+            throw new IllegalArgumentException("The given set cannot be null");
+        }
+
+        // total number of subsets (2^n)
+        long subsetsNo = (long) Math.pow(2, set.length);
+
+        // store subsets
+        Set<Set<Character>> subsets = new HashSet<>();
+
+        // generate each subset one by one
+        for (int i = 0; i < subsetsNo; i++) {
+            Set<Character> subset = new HashSet<>();
+
+            // check every bit of i
+            for (int j = 0; j < set.length; j++) {
+                // if j'th bit of i is set, add set[j] to the current subset
+                if ((i & (1 << j)) != 0) {
+                    subset.add(set[j]);
+                }
+            }
+
+            subsets.add(subset);
+        }
+
+        return subsets;
+    }
+}
