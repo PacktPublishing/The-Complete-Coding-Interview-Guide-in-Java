00c8ef5cfe9f55345bc2927e9b96b6aaafe1dde6
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-08 14:33:30 +0300

    Reverse list in k groups (Adobe, Amazon, Microsoft)

---
+++ Chapter11/ReverseLinkedListInGroups/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,25 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       SinglyLinkedList sll = new SinglyLinkedList();              
+       
+       sll.insertFirst(0);       
+       sll.insertFirst(9);       
+       sll.insertFirst(2);       
+       sll.insertFirst(8);       
+       sll.insertFirst(1);
+       sll.insertFirst(3);
+       sll.insertFirst(4);
+       sll.insertFirst(7);
+              
+       sll.print();
+       
+       sll.reverseInKGroups(3);
+       
+       sll.print();       
+    }
+
+}
---
+++ Chapter11/ReverseLinkedListInGroups/src/main/java/coding/challenge/SinglyLinkedList.java
@@ -0,0 +1,81 @@
+package coding.challenge;
+
+public final class SinglyLinkedList {
+
+    private final class Node {
+
+        private int data;
+        private Node next;
+
+        @Override
+        public String toString() {
+            return " " + data + " ";
+        }
+    }
+
+    private Node head;
+
+    public void insertFirst(int data) {
+
+        Node newNode = new Node();
+
+        newNode.data = data;
+
+        newNode.next = head;
+
+        head = newNode;
+    }
+
+    public void reverseInKGroups(int k) {
+
+        if (k <= 0) {
+            throw new IllegalArgumentException("A group cannot be 0 or negative");
+        }
+
+        if (head != null) {
+            head = reverseInKGroups(head, k);
+        }
+    }
+
+    private Node reverseInKGroups(Node head, int k) {
+
+        Node current = head;
+        Node next = null;
+        Node prev = null;
+
+        int counter = 0;
+
+        // reverse first 'k' nodes of linked list
+        while (current != null && counter < k) {
+
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+
+            counter++;
+        }
+
+        // 'next' points to (k+1)th node            
+        if (next != null) {
+            head.next = reverseInKGroups(next, k);
+        }
+
+        // 'prev' is now the head of the input list 
+        return prev;
+    }
+
+    public void print() {
+
+        System.out.println("\nHead (" + head + ") ----------> Last:");
+
+        Node currentNode = head;
+        while (currentNode != null) {
+
+            System.out.print(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        System.out.println();
+    }
+}
