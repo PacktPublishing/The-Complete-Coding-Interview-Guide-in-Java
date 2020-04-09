5702b1a4e07427f2f6e45c4643bfe6a6c8841f79
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-09 15:36:13 +0300

    Container with most water (Amazon, Facebook, Google)

---
+++ Chapter10/ContainerMostWater/src/main/java/coding/challenge/Containers.java
@@ -0,0 +1,55 @@
+package coding.challenge;
+
+public final class Containers {
+
+    private Containers() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    /* O(n^2) */
+    public static int maxArea(int[] heights) {
+
+        if (heights == null) {
+            throw new IllegalArgumentException("The given array cannot be null");
+        }
+
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+                // traverse each (i, j) pair
+                maxArea = Math.max(maxArea, Math.min(heights[i], heights[j]) * (j - i));
+            }
+        }
+
+        return maxArea;
+    }
+
+    /* O(n) */
+    public static int maxAreaOptimized(int[] heights) {
+
+        if (heights == null) {
+            throw new IllegalArgumentException("The given array cannot be null");
+        }
+
+        int maxArea = 0;
+
+        int i = 0; // left-hand side pointer            
+        int j = heights.length - 1; // right-hand side pointer
+
+        // area cannot be negative, therefore i should not be greater than j
+        while (i < j) {
+
+            // calculate area for each pair
+            maxArea = Math.max(maxArea, Math.min(heights[i], heights[j]) * (j - i));
+
+            if (heights[i] <= heights[j]) {
+                i++; // left pointer is small than right pointer
+            } else {
+                j--; // right pointer is small than left pointer
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ Chapter10/ContainerMostWater/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,16 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] heights = {1, 4, 6, 2, 7, 3, 8, 5, 3};
+
+        int max1 = Containers.maxArea(heights);
+        int max2 = Containers.maxAreaOptimized(heights);
+        
+        System.out.println("Result 1: " + max1);
+        System.out.println("Result 2: " + max2);
+    }
+
+}
