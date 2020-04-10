1cf23cd17b563c8769b4aa7e4ef4c9796b5bc642
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-10 13:41:26 +0300

    Longest consecutive sequence (Amazon, Microsoft)

---
+++ Chapter10/LongestConsecutiveSequence/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,13 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] sequence = {4, 2, 9, 5, 12, 6, 8};
+
+        int longestSequence = Sequences.findLongestConsecutive(sequence);
+        System.out.println("Longest sequence has " + longestSequence + " element(s)");
+    }
+
+}
---
+++ Chapter10/LongestConsecutiveSequence/src/main/java/coding/challenge/Sequences.java
@@ -0,0 +1,45 @@
+package coding.challenge;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class Sequences {
+
+    private Sequences() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int findLongestConsecutive(int[] sequence) {
+
+        if (sequence == null) {
+            throw new IllegalArgumentException("The given sequence cannot be empty");
+        }
+
+        // construct a set from the given sequence
+        Set<Integer> sequenceSet = IntStream.of(sequence)
+                .boxed()
+                .collect(Collectors.toSet());
+
+        int longestSequence = 1;
+
+        for (int elem : sequence) {
+
+            // if 'elem-1' is not in the set then start a new sequence
+            if (!sequenceSet.contains(elem - 1)) {
+                
+                int sequenceLength = 1;
+
+                // lookup in the set for elements 'elem + 1', 'elem + 2', 'elem + 3' ...
+                while (sequenceSet.contains(elem + sequenceLength)) {
+                    sequenceLength++;
+                }
+
+                // update the longest consecutive subsequence
+                longestSequence = Math.max(longestSequence, sequenceLength);
+            }
+        }
+
+        return longestSequence;
+    }
+}
