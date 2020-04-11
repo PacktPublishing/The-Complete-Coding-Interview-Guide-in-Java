d30663968a2658d51ec15143d5b7c8ab0c240cd7
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-11 09:11:36 +0300

    Petrol bunks (Amazon, Flipkart, Microsoft, Google)

---
+++ Chapter10/PetrolBunks/src/main/java/coding/challenge/Bunks.java
@@ -0,0 +1,39 @@
+package coding.challenge;
+
+public final class Bunks {
+
+    private Bunks() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+    
+    /* O(n) */
+    public static int circularTour(int[] fuel, int[] dist) {
+
+        int sumRemainingFuel = 0; // track current remaining fuel
+        int totalFuel = 0;        // track total remaining fuel
+        int start = 0;
+
+        for (int i = 0; i < fuel.length; i++) {
+
+            int remainingFuel = fuel[i] - dist[i];
+
+            //if sum remaining fuel of (i-1) >= 0 then continue 
+            if (sumRemainingFuel >= 0) {
+                sumRemainingFuel += remainingFuel;
+
+                //otherwise, reset start index to be current
+            } else {
+                sumRemainingFuel = remainingFuel;
+                start = i;
+            }
+
+            totalFuel += remainingFuel;
+        }
+
+        if (totalFuel >= 0) {
+            return start;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ Chapter10/PetrolBunks/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,14 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] dist = {5, 4, 6, 3, 5, 7};
+        int[] fuel = {3, 3, 5, 5, 6, 8};
+
+        int startPoint = Bunks.circularTour(fuel, dist);
+        System.out.println("Start point: " + startPoint);
+    }
+
+}
