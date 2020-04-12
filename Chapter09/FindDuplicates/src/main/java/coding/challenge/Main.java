2bd70863aefd8c585fb45c52872905b847dde35b
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-12 13:29:52 +0300

    Find duplicates (Google, Amazon)

--- Chapter09/FindDuplicates/src/main/java/coding/challenge/Main.java
+++ Chapter09/FindDuplicates/src/main/java/coding/challenge/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = {0, 4, 2, 3, 1, 5, 6, 2, 3, 12, 0, 4, 1, 100, 35000, 35000};
+        int[] numbers = {0, 4, 2, 3, 1, 5, 6, 2, 3, 12, 0, 4, 1};
         
         Bits.printDuplicates(numbers);
     }
