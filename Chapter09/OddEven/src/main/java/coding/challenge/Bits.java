354bd3c94d5a523b7706b64810531fed61519d0f
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-12 14:51:44 +0300

    Odd even number

--- Chapter09/OddEven/src/main/java/coding/challenge/Bits.java
+++ Chapter09/OddEven/src/main/java/coding/challenge/Bits.java
@@ -5,8 +5,7 @@ public final class Bits {
     private Bits() {
         throw new AssertionError("Cannot be instantiated");
     }
-    
-    // 'q' must be power of 2
+       
     public static boolean isOdd(int p) {
         
         return ((p & 1) != 0);
