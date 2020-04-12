4d01b3f5a744bb557357bd7bf3c9d96d9a541cf5
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-12 13:28:15 +0300

    Find duplicates (Google, Amazon)

---
+++ Chapter09/FindDuplicates/src/main/java/coding/challenge/Bits.java
@@ -0,0 +1,27 @@
+package coding.challenge;
+
+import java.util.BitSet;
+
+public final class Bits {
+
+    private static final int MAX_N = 32000;
+
+    private Bits() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static void printDuplicates(int[] arr) {
+
+        BitSet bitArr = new BitSet(MAX_N);
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int nr = arr[i];
+            if (bitArr.get(nr)) {                
+                System.out.println("Duplicate: " + nr);
+            } else {
+                bitArr.set(nr);                
+            }
+        }
+    }
+}
---
+++ Chapter09/FindDuplicates/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,12 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {0, 4, 2, 3, 1, 5, 6, 2, 3, 12, 0, 4, 1, 100, 35000, 35000};
+        
+        Bits.printDuplicates(numbers);
+    }
+
+}
