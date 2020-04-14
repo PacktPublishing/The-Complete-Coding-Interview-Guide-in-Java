7165416e2ff705fa3df454a399f2e6041e917411
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-14 08:22:47 +0300

    Number divisible by digits

---
+++ Chapter15/NumberDivisibleDigits/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,17 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("1: " + Numbers.isDivisible(-4));
+        System.out.println("6242: " + Numbers.isDivisible(6242));
+        System.out.println("123: " + Numbers.isDivisible(123));
+        System.out.println("46: " + Numbers.isDivisible(46));
+        System.out.println("144: " + Numbers.isDivisible(144));
+        System.out.println("250: " + Numbers.isDivisible(250));
+        System.out.println("1030: " + Numbers.isDivisible(1030));
+        System.out.println("1010: " + Numbers.isDivisible(1010));
+    }
+}
+
---
+++ Chapter15/NumberDivisibleDigits/src/main/java/coding/challenge/Numbers.java
@@ -0,0 +1,26 @@
+package coding.challenge;
+
+public final class Numbers {
+
+    private Numbers() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static boolean isDivisible(int n) {
+
+        int t = n;
+
+        while (n > 0) {
+
+            int k = n % 10;
+
+            if (k != 0 && t % k != 0) {
+                return false;
+            }
+
+            n /= 10;
+        }
+
+        return true;
+    }
+}
