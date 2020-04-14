b6660a14b4939d893beee1fd8a00c3d71fd9c359
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-14 17:57:31 +0300

    Prefix expressions (Google, Amazon)

--- Chapter12/PrefixExpressions/src/main/java/coding/challenge/Main.java
+++ Chapter12/PrefixExpressions/src/main/java/coding/challenge/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // - * / 15 - 7 + 1 1 3 + 2 + 1 1
+        // ((15 / (7 - (1 + 1))) * 3) - (2 + 1 + 1) = 5
         String exp = "- * / 15 - 7 + 1 1 3 + 2 + 1 1";
         System.out.println("Prefix Expression: " + exp);
         System.out.println("Evaluation: " + Expressions.convert(exp));
