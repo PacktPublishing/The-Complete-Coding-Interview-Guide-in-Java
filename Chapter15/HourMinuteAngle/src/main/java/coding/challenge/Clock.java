05919730f649fdbb55d5253d174adda60cf5a79e
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-14 17:04:10 +0300

    Clock angle (Google, Facebook)

---
+++ Chapter15/HourMinuteAngle/src/main/java/coding/challenge/Clock.java
@@ -0,0 +1,23 @@
+package coding.challenge;
+
+public final class Clock {
+
+    private Clock() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static float findAngle(int hour, int min) {
+
+        if (hour <= 0 || hour > 12) {
+            throw new IllegalArgumentException("Hour must be between 1-12");
+        }
+
+        if (min < 0 || min > 60) {
+            throw new IllegalArgumentException("Minute must be between 0-60");
+        }
+
+        float angle = (float) Math.abs(((30f * hour) + (0.5f * min)) - (6f * min));
+
+        return angle > 180f ? (360f - angle) : angle;
+    }
+}
---
+++ Chapter15/HourMinuteAngle/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,20 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float clock1 = Clock.findAngle(10, 10);
+        float clock2 = Clock.findAngle(9, 40);
+        float clock3 = Clock.findAngle(4, 40);
+        
+        float clock4 = Clock.findAngle(12, 1);
+        float clock5 = Clock.findAngle(4, 19);
+        
+        System.out.println("10:10 has an angle of " + clock1 + " degrees");
+        System.out.println("9:40 has an angle of " + clock2 + " degrees");
+        System.out.println("4:40 has an angle of " + clock3 + " degrees");        
+        System.out.println("12:01 has an angle of " + clock4 + " degrees");        
+        System.out.println("4:19 has an angle of " + clock5 + " degrees");        
+    }
+}
