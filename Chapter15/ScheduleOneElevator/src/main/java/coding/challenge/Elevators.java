7f81577d85918a2e2c244c43d7f7beea2297c980
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-17 11:20:53 +0300

    Schedule one elevator (Google, Amazon, Facebook)

---
+++ Chapter15/ScheduleOneElevator/src/main/java/coding/challenge/Elevators.java
@@ -0,0 +1,34 @@
+package coding.challenge;
+
+public final class Elevators {
+
+    private Elevators() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static int time(int k, int floors[]) {
+
+        if (floors == null || floors.length == 0 || k <= 0) {
+            return -1;
+        }
+
+        int aux;
+        for (int i = 0; i < floors.length - 1; i++) {
+            for (int j = i + 1; j < floors.length; j++) {
+                if (floors[i] < floors[j]) {
+                    aux = floors[i];
+                    floors[i] = floors[j];
+                    floors[j] = aux;
+                }
+            }
+        }
+
+        // iterate the groups and update the time needed for each group 
+        int time = 0;
+        for (int i = 0; i < floors.length; i += k) {
+            time += (2 * floors[i]);
+        }
+
+        return time;
+    }
+}
---
+++ Chapter15/ScheduleOneElevator/src/main/java/coding/challenge/Main.java
@@ -0,0 +1,12 @@
+package coding.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int k = 3;
+        int floors[] = {4, 2, 1, 2, 4};
+        
+        System.out.println("Minimum time: " + Elevators.time(k, floors));
+    }
+}
