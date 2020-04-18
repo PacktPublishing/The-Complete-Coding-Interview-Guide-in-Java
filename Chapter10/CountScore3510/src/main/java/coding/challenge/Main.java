ec25486234814d1df1fe1e35110d3c49d760a33a
AnghelLeonard <leoprivacy@yahoo.com>
2020-04-18 09:08:32 +0300

    Count game score (Amazon, Google, Microsoft)

--- Chapter10/CountScore3510/src/main/java/coding/challenge/Main.java
+++ Chapter10/CountScore3510/src/main/java/coding/challenge/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("33: " + Scores.count(20));
+        System.out.println("33: " + Scores.count(33));
         System.out.println("18: " + Scores.count(18));
     }
 
