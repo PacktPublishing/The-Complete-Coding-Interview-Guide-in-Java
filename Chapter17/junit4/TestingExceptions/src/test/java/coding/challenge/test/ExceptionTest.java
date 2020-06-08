b4dd15f5f2f3e6a03e8644f3c04908de9fcbf6fa
AnghelLeonard <leoprivacy@yahoo.com>
2020-06-08 09:48:18 +0300

    Testing exceptions

---
+++ Chapter17/junit4/TestingExceptions/src/test/java/coding/challenge/test/ExceptionTest.java
@@ -0,0 +1,51 @@
+package coding.challenge.test;
+
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class ExceptionTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    // Try/Catch Idiom
+    @Test
+    public void givenStreamWhenGetThenException1() {
+
+        Stream<Integer> theStream = Stream.of();
+
+        try {
+            theStream.findAny().get();
+            fail("Expected an java.util.NoSuchElementException to be thrown");
+        } catch (NoSuchElementException ex) {
+            assertThat(ex.getMessage(), is("No value present"));
+        }
+    }
+
+    // @Test(expected=...)    
+    @Test(expected = NoSuchElementException.class)
+    public void givenStreamWhenGetThenException2() {
+
+        Stream<Integer> theStream = Stream.of();
+
+        theStream.findAny().get();
+    }
+
+    // @Rule
+    @Test
+    public void givenStreamWhenGetThenException3() throws NoSuchElementException {
+
+        Stream<Integer> theStream = Stream.of();
+
+        thrown.expect(NoSuchElementException.class);
+        thrown.expectMessage("No value present");
+
+        theStream.findAny().get();
+    }
+}
