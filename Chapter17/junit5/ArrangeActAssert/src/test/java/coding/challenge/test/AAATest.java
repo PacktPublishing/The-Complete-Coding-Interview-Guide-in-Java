dc97b0026cd52d4076de79871f39109dd8a0793d
AnghelLeonard <leoprivacy@yahoo.com>
2020-06-08 11:45:08 +0300

    Arrange-Act-Assert

--- Chapter17/junit5/ArrangeActAssert/src/test/java/coding/challenge/test/AAATest.java
+++ Chapter17/junit5/ArrangeActAssert/src/test/java/coding/challenge/test/AAATest.java
@@ -4,6 +4,10 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
 public class AAATest {
 
     @Test
