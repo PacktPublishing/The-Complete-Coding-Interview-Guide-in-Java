b177d82bd9c2f48a3ef21ba8920583e119725ab1
AnghelLeonard <leoprivacy@yahoo.com>
2020-06-09 08:57:41 +0300

    Assumptions

---
+++ Chapter17/junit4/Assumptions/src/test/java/coding/challenge/test/AssumptionsTest.java
@@ -0,0 +1,21 @@
+package coding.challenge.test;
+
+import java.io.File;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AssumptionsTest {
+     
+    @Test
+    public void givenCurrentFolderWhenGetAbsolutePathThenSuccess() {
+
+        assumeThat(File.separatorChar, is('/'));
+        assertThat(new File(".").getAbsolutePath(), is("C:/SBPBP/GitHub//Chapter17/junit4"));
+    }
+
+}
