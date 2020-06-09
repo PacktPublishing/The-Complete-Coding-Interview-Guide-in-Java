e1013fcbcdd23193b232dc81231c5a8686c00f4d
AnghelLeonard <leoprivacy@yahoo.com>
2020-06-09 10:34:07 +0300

    Dynamic tests

---
+++ Chapter17/junit5/TestFactory/src/test/java/coding/challenge/test/ExampleOfDynamicTest.java
@@ -0,0 +1,35 @@
+package coding.challenge.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DynamicTest;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class ExampleOfDynamicTest {
+
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsExample() {
+
+        List<Integer> items = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for (int item : items) {
+            DynamicTest dynamicTest = dynamicTest(
+                    "pow(" + item + ", 2):", () -> {
+                        assertEquals(item * item, Math.pow(item, 2));
+                    });
+
+            dynamicTests.add(dynamicTest);
+        }
+
+        return dynamicTests.stream();
+    }
+}
