f5b83b70f899749b4b1bc482ca7eff2916611310
AnghelLeonard <leoprivacy@yahoo.com>
2020-06-09 09:15:27 +0300

    Assumptions

---
+++ Chapter17/junit5/Assumptions/src/test/java/coding/challenge/test/AssumptionsTest.java
@@ -0,0 +1,27 @@
+package coding.challenge.test;
+
+import java.io.File;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class AssumptionsTest {
+
+    @Test
+    public void givenCurrentFolderWhenGetAbsolutePathThenSuccess() {
+
+        assumingThat(File.separatorChar == '/',
+                () -> {
+                    assertThat(new File(".").getAbsolutePath(), 
+                            is("C:/SBPBP/GitHub/Chapter17/junit5"));
+                });
+        
+        // run these assertions always, just like normal test
+        assertTrue(true);
+    }
+}
