4f8dd47569c6549c3bbd3b2aba0e81a545671aaf
AnghelLeonard <leoprivacy@yahoo.com>
2020-08-15 12:08:00 +0300

    Vending machine (Amazon, Google, Adobe)

--- Chapter06/VendingMachine/src/main/java/coding/challenge/Main.java
+++ Chapter06/VendingMachine/src/main/java/coding/challenge/Main.java
@@ -3,7 +3,7 @@ package coding.challenge;
 import coding.challenge.vending.machine.VendingMachine;
 
 public class Main {
-
+ 
     public static void main(String[] args) {
 
         VendingMachine vm = new VendingMachine();        
